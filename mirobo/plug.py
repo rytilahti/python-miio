cea4e70d77d8f7a3fac7d86f6f4ec2453381a0d9
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-07-19 22:08:04 +0200

    Code reformatted.

--- mirobo/plug.py
+++ mirobo/plug.py
@@ -1,8 +1,9 @@
-from .device import Device, DeviceException
+from .device import Device
 
 PROPERTY_POWER = 'power'
 PROPERTY_TEMPERATURE = 'temperature'
 PROPERTY_CURRENT = 'current'
+
 
 class Plug(Device):
     """Main class representing the smart wifi socket / plug."""
@@ -21,8 +22,15 @@ class Plug(Device):
 
     def status(self):
         """Retrieve properties."""
-        status =  self.send("get_prop", [PROPERTY_POWER, PROPERTY_TEMPERATURE, PROPERTY_CURRENT])
-        return {PROPERTY_POWER: status[0], PROPERTY_TEMPERATURE: status[1], PROPERTY_CURRENT: status[2]}
+        status =  self.send(
+            "get_prop",
+            [ PROPERTY_POWER, PROPERTY_TEMPERATURE, PROPERTY_CURRENT ]
+        )
+        return {
+            PROPERTY_POWER: status[0],
+            PROPERTY_TEMPERATURE: status[1],
+            PROPERTY_CURRENT: status[2]
+        }
 
     def raw_command(self, cmd, params):
         """Send a raw command to the device."""
