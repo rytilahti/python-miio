bea60f6df63d1a9d34c4e4207d0eadc3103c11fe
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-07-19 23:54:54 +0200

    Some refactoring.

--- mirobo/plug.py
+++ mirobo/plug.py
@@ -1,16 +1,8 @@
 from .device import Device
-
-PROPERTY_POWER = 'power'
-PROPERTY_TEMPERATURE = 'temperature'
-PROPERTY_CURRENT = 'current'
-
+from .containers import PlugStatus
 
 class Plug(Device):
     """Main class representing the smart wifi socket / plug."""
-
-    def __init__(self, ip: str, token: str, start_id: int = 0, debug: int = 0) -> None:
-        super().__init__(ip, token, start_id, debug)
-        self.manual_seqnum = -1
 
     def start(self):
         """Power on."""
@@ -22,16 +14,9 @@ class Plug(Device):
 
     def status(self):
         """Retrieve properties."""
-        status = self.send(
+        properties = ['power', 'temperature', 'current']
+        values = self.send(
             "get_prop",
-            [PROPERTY_POWER, PROPERTY_TEMPERATURE, PROPERTY_CURRENT]
+            properties
         )
-        return {
-            PROPERTY_POWER: status[0],
-            PROPERTY_TEMPERATURE: status[1],
-            PROPERTY_CURRENT: status[2]
-        }
-
-    def raw_command(self, cmd, params):
-        """Send a raw command to the device."""
-        return self.send(cmd, params)
+        return PlugStatus(dict(zip(properties, values)))
