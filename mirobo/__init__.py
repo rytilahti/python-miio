0178c1bfcea3da3aa143a3d33335950d090ae5cb
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-07-23 23:51:29 +0200

    Strip added to __init__.

--- mirobo/__init__.py
+++ mirobo/__init__.py
@@ -3,4 +3,5 @@ from mirobo.protocol import Message, Utils
 from mirobo.containers import VacuumStatus, ConsumableStatus, CleaningDetails, CleaningSummary, Timer
 from mirobo.vacuum import Vacuum, VacuumException
 from mirobo.plug import Plug
+from mirobo.strip import Strip
 from mirobo.device import Device, DeviceException
