3f4ee1c67bc89ce0ff1706b450ccd183ff5265fe
Teemu Rytilahti <tpr@iki.fi>
2017-08-01 22:00:02 +0200

    move PlugStatus to plug.py, rename containers to vacuumcontainers

--- mirobo/__init__.py
+++ mirobo/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from mirobo.protocol import Message, Utils
-from mirobo.containers import VacuumStatus, ConsumableStatus, CleaningDetails, CleaningSummary, Timer
+from mirobo.vacuumcontainers import VacuumStatus, ConsumableStatus, CleaningDetails, CleaningSummary, Timer
 from mirobo.vacuum import Vacuum, VacuumException
 from mirobo.plug import Plug
 from mirobo.strip import Strip
