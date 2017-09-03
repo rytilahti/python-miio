85ecdf4b5e2f891854aef7e21c1b902ba3bec984
Teemu Rytilahti <teemu.rytilahti@rub.de>
2017-09-03 22:04:32 +0300

    add some initial unit tests

---
+++ mirobo/tests/test_protocol.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from .. import Message, Utils
+
+
+class TestProtocol(TestCase):
+    def test_non_bytes_payload(self):
+        payload = "hello world"
+        valid_token = 32 * b'0'
+        with self.assertRaises(TypeError):
+            Utils.encrypt(payload, valid_token)
+        with self.assertRaises(TypeError):
+            Utils.decrypt(payload, valid_token)
+
+    def test_encrypt(self):
+        payload = b"hello world"
+        token = 32 * b'0'
+
+        encrypted = Utils.encrypt(payload, token)
+        decrypted = Utils.decrypt(encrypted, token)
+        self.assertEquals(payload, decrypted)
+
+    def test_invalid_token(self):
+        payload = b"hello world"
+        wrong_type = 1234
+        wrong_length = 31 * b'0'
+        with self.assertRaises(TypeError):
+            Utils.encrypt(payload, wrong_type)
+        with self.assertRaises(TypeError):
+            Utils.decrypt(payload, wrong_type)
+
+        with self.assertRaises(ValueError):
+            Utils.encrypt(payload, wrong_length)
+        with self.assertRaises(ValueError):
+            Utils.decrypt(payload, wrong_length)
