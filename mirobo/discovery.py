89f4a6caf0a0654b2faf4e2dee07fe741318bce4
Sebastian Muszynski <basti@linkt.de>
2017-09-03 21:29:37 +0200

    mDNS device map extended (#51)
    * mDNS device map extended.
    
    * Missing imports added.
    
    * Reformat.
    
    * Unused imports removed.
    
    * Missing spaces added.

--- mirobo/discovery.py
+++ mirobo/discovery.py
@@ -3,8 +3,9 @@ import zeroconf
 import ipaddress
 import inspect
 import codecs
+from mirobo import (Device, Vacuum, Plug, PlugV1, Strip, AirPurifier, Ceil,
+                    PhilipsEyecare)
 from typing import Union, Callable, Dict
-from mirobo import Device, Vacuum
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -49,6 +50,22 @@ class Listener:
 class Discovery:
     _mdns_device_map = {
         "rockrobo-vacuum-v1": Vacuum,
+        "chuangmi-plug-m1": Plug,
+        "chuangmi-plug-v2": Plug,
+        "chuangmi-plug-v1": PlugV1,
+        "qmi-powerstrip-v1": Strip,
+        "zimi.powerstrip.v2": Strip,
+        "zhimi-airpurifier-m1": AirPurifier,
+        "zhimi-airpurifier-v1": AirPurifier,
+        "zhimi-airpurifier-v2": AirPurifier,
+        "zhimi-airpurifier-v3": AirPurifier,
+        "zhimi-airpurifier-v6": AirPurifier,
+        # "zhimi-humidifier-v1": Humidifier,
+        # "yunmi-waterpuri-v2": WaterPurifier,
+        # It looks like philips devices cannot be discovered via mdns
+        "philips-light-bulb": Ceil,
+        "philips-light-ceil": Ceil,
+        "philips-light-sread1": PhilipsEyecare,
         "yeelink-light-": lambda x: other_package_info(x, "python-yeelight package"),
         "lumi-gateway-": lambda x: other_package_info(x, "https://github.com/Danielhiversen/PyXiaomiGateway")
     } # type: Dict[str, Union[Callable, Device]]
