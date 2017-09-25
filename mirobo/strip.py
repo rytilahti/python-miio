8b6496c198de7a26773dba49c8ea369d65ee4922
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-09-25 08:23:04 +0200

    Findings of the Xiaomi Plug device support ported to the Xiaomi Power Strip.

--- mirobo/strip.py
+++ mirobo/strip.py
@@ -1,11 +1,59 @@
-from .device import Device
-from typing import Any, Dict
+import logging
 import enum
+from typing import Dict, Any, Optional
+from collections import defaultdict
+from .device import Device
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class PowerMode(enum.Enum):
     Eco = 'green'
     Normal = 'normal'
+
+
+class StripStatus:
+    """Container for status reports from the strip."""
+
+    def __init__(self, data: Dict[str, Any]) -> None:
+        # Device model: qmi.powerstrip.v1, zimi.powerstrip.v2
+        #
+        # {'power': 'on', 'temperature': 48.11,
+        # 'current': 0.06, 'mode': 'green'}
+        self.data = data
+
+    @property
+    def power(self) -> str:
+        return self.data["power"]
+
+    @property
+    def is_on(self) -> bool:
+        return self.power == "on"
+
+    @property
+    def temperature(self) -> float:
+        return self.data["temperature"]
+
+    @property
+    def load_power(self) -> Optional[float]:
+        if self.data["current"] is not None:
+            # The constant of 110V is used intentionally. The current was
+            # calculated with a wrong reference voltage already.
+            return self.data["current"] * 110
+        return None
+
+    @property
+    def mode(self) -> PowerMode:
+        return PowerMode(self.data["mode"])
+
+    def __str__(self) -> str:
+        s = "<StripStatus power=%s, temperature=%s, " \
+            "load_power=%s mode=%s>" % \
+            (self.power,
+             self.temperature,
+             self.load_power,
+             self.mode)
+        return s
 
 
 class Strip(Device):
@@ -26,46 +74,20 @@ class Strip(Device):
             "get_prop",
             properties
         )
-        return StripStatus(dict(zip(properties, values)))
+
+        properties_count = len(properties)
+        values_count = len(values)
+        if properties_count != values_count:
+            _LOGGER.debug(
+                "Count (%s) of requested properties does not match the "
+                "count (%s) of received values.",
+                properties_count, values_count)
+
+        return StripStatus(
+            defaultdict(lambda: None, zip(properties, values)))
 
     def set_power_mode(self, mode: PowerMode):
         """Set mode."""
 
         # green, normal
         return self.send("set_power_mode", [mode.value])
-
-
-class StripStatus:
-    """Container for status reports from the strip."""
-
-    def __init__(self, data: Dict[str, Any]) -> None:
-        # {'power': 'on', 'temperature': 48.11,
-        # 'current': 0.06, 'mode': 'green'}
-        self.data = data
-
-    @property
-    def power(self) -> str:
-        return self.data["power"]
-
-    @property
-    def is_on(self) -> bool:
-        return self.power == "on"
-
-    @property
-    def temperature(self) -> float:
-        return self.data["temperature"]
-
-    @property
-    def current(self) -> float:
-        return self.data["current"]
-
-    @property
-    def mode(self) -> PowerMode:
-        return PowerMode(self.data["mode"])
-
-    def __str__(self) -> str:
-        s = "<StripStatus power=%s, temperature=%s, " \
-            "current=%s mode=%s>" % \
-            (self.power, self.temperature,
-             self.current, self.mode)
-        return s
