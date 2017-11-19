beeef6ae874512bdeae1377e1f97cf87d3e11e31
Sebastian Muszynski <basti@linkt.de>
2017-11-19 16:58:50 +0100

    Load power of the PowerStrip fixed and removed from the Plug (#117)
    * Property load_power removed from the plug. It's a feature of the PowerStrip. The Xiaomi plug doesn't provide load.
    Load power workaround of the PowerStrip replaced by a property called "power_consume_rate" which provides the same
    values as the android app.
    
    * Line too long fixed.
    
    * Unittest for the PowerStrip added.
    Unittest of the Plug updated.
    
    * Fixture updated.
    
    * Fixture updated. Missing property ("mode") added.
    
    * Make hound happy again.
    
    * The hound error is fine for better readability of the assertion.

--- miio/tests/test_powerstrip.py
+++ miio/tests/test_powerstrip.py
@@ -1,14 +1,17 @@
 from unittest import TestCase
-from miio import Plug
+from miio import PowerStrip
 from .dummies import DummyDevice
 import pytest
 
 
-class DummyPlug(DummyDevice, Plug):
+class DummyPowerStrip(DummyDevice, PowerStrip):
     def __init__(self, *args, **kwargs):
         self.state = {
             'power': 'on',
-            'temperature': 32,
+            'mode': 'normal',
+            'temperature': 32.5,
+            'current': 123,
+            'power_consume_rate': 12.5,
         }
         self.return_values = {
             'get_prop': self._get_state,
@@ -18,13 +21,13 @@ class DummyPlug(DummyDevice, Plug):
 
 
 @pytest.fixture(scope="class")
-def plug(request):
-    request.cls.device = DummyPlug()
+def powerstrip(request):
+    request.cls.device = DummyPowerStrip()
     # TODO add ability to test on a real device
 
 
-@pytest.mark.usefixtures("plug")
-class TestPlug(TestCase):
+@pytest.mark.usefixtures("powerstrip")
+class TestPowerStrip(TestCase):
     def is_on(self):
         return self.device.status().is_on
 
@@ -52,3 +55,9 @@ class TestPlug(TestCase):
 
         assert self.is_on() is True
         assert self.state().temperature == self.device.start_state["temperature"]
+        assert self.state().load_power == self.device.start_state["power_consume_rate"]
+
+    def test_status_without_power_consume_rate(self):
+        del self.device.state["power_consume_rate"]
+
+        assert self.state().load_power is None
