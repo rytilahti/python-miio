db8b4b5beb6dd1969572651a1bae79aa82d47d46
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-01-22 10:19:14 +0100

    Example payload updated.

--- miio/airpurifier.py
+++ miio/airpurifier.py
@@ -29,16 +29,16 @@ class AirPurifierStatus:
 
     def __init__(self, data: Dict[str, Any]) -> None:
         """
-        Response of a Air Purifier Pro:
+        Response of a Air Purifier Pro (zhimi.airpurifier.v6):
 
-        ['power': 'off', 'aqi': 41, 'humidity': 62, 'temp_dec': 293,
-         'mode': 'auto', 'led': 'on', 'led_b': null, 'buzzer': null,
-         'child_lock': 'off', 'limit_hum': null, 'trans_level': null,
-         'bright': 71, 'favorite_level': 17, 'filter1_life': 77,
-         'act_det': null, 'f1_hour_used': 771, 'use_time': 2776200,
-         'motor1_speed': 0, 'volume': 100]
+        {'power': 'off', 'aqi': 7, 'average_aqi': 18, 'humidity': 45,
+         'temp_dec': 234, 'mode': 'auto', 'favorite_level': 17,
+         'filter1_life': 52, 'f1_hour_used': 1664, 'use_time': 2642700,
+         'motor1_speed': 0, 'purify_volume': 62180, 'f1_hour': 3500,
+         'led': 'on', 'led_b': None, 'bright': 83, 'buzzer': None,
+         'child_lock': 'off', 'volume': 50}
 
-        Response of a Air Purifier 2:
+        Response of a Air Purifier 2 (zhimi.airpurifier.m1):
 
         {'power': 'on, 'aqi': 10, 'average_aqi': 8, 'humidity': 62,
          'temp_dec': 186, 'mode': 'auto', 'favorite_level': 10,
