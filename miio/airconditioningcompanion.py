f7c0edb52b55c8e9677f41631f07eebdc70ae241
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-01-26 08:01:47 +0100

    Marker format updated.

--- miio/airconditioningcompanion.py
+++ miio/airconditioningcompanion.py
@@ -32,40 +32,40 @@ STORAGE_SLOT_ID = 30
 POWER_OFF = 'off'
 
 # Command templates per model number (f.e. 0180111111)
-# po, mo, wi, sw, tt, t1t, t4t and t7t are markers which will be replaced
+# [po], [mo], [wi], [sw], [tt], [tt1], [tt4] and [tt7] are markers which will be replaced
 DEVICE_COMMAND_TEMPLATES = {
     'fallback': {
         'deviceType': 'generic',
-        'base': 'pomowiswtta0'
+        'base': '[po][mo][wi][sw][tt]a0'
     },
     '0180111111': {
         'deviceType': 'media_1',
-        'base': 'pomowiswtt02'
+        'base': '[po][mo][wi][sw][tt]02'
     },
     '0180222221': {
         'deviceType': 'gree_1',
-        'base': 'pomowiswtt02'
+        'base': '[po][mo][wi][sw][tt]02'
     },
     '0100010727': {
         'deviceType': 'gree_2',
-        'base': 'pomowiswtt1100190t1t205002102000t7t0190t1t207002000000t4t0',
+        'base': '[po][mo][wi][sw][tt]1100190[tt1]205002102000t7t0190[tt1]207002000000[tt4]0',
         'off': '01011101004000205002112000D04000207002000000A0'
     },
     '0100004795': {
         'deviceType': 'gree_8',
-        'base': 'pomowiswtt0100090900005002'
+        'base': '[po][mo][wi][sw][tt]0100090900005002'
     },
     '0180333331': {
         'deviceType': 'haier_1',
-        'base': 'pomowiswtt12'
+        'base': '[po][mo][wi][sw][tt]12'
     },
     '0180666661': {
         'deviceType': 'aux_1',
-        'base': 'pomowiswtt12'
+        'base': '[po][mo][wi][sw][tt]12'
     },
     '0180777771': {
         'deviceType': 'chigo_1',
-        'base': 'pomowiswtt12'
+        'base': '[po][mo][wi][sw][tt]12'
     }
 }
 
@@ -177,23 +177,23 @@ class AirConditioningCompanion(Device):
         else:
             configuration = model + DEVICE_COMMAND_TEMPLATES['fallback']['base']
 
-        configuration = configuration.replace('po', power.value)
-        configuration = configuration.replace('mo', operation_mode.value)
-        configuration = configuration.replace('wi', fan_speed.value)
-        configuration = configuration.replace('sw', swing_mode.value)
+        configuration = configuration.replace('[po]', power.value)
+        configuration = configuration.replace('[mo]', operation_mode.value)
+        configuration = configuration.replace('[wi]', fan_speed.value)
+        configuration = configuration.replace('[sw]', swing_mode.value)
         configuration = configuration.replace(
-            'tt', hex(int(target_temperature))[2:])
+            '[tt]', hex(int(target_temperature))[2:])
 
         temperature = (1 + int(target_temperature) - 17) % 16
         temperature = hex(temperature)[2:].upper()
-        configuration = configuration.replace('t1t', temperature)
+        configuration = configuration.replace('[[tt1]]', temperature)
 
         temperature = (4 + int(target_temperature) - 17) % 16
         temperature = hex(temperature)[2:].upper()
-        configuration = configuration.replace('t4t', temperature)
+        configuration = configuration.replace('[tt4]', temperature)
 
         temperature = (7 + int(target_temperature) - 17) % 16
         temperature = hex(temperature)[2:].upper()
-        configuration = configuration.replace('t7t', temperature)
+        configuration = configuration.replace('[tt7]', temperature)
 
         return self.send_command(configuration)
