82d85ae954fb46cc23e54151e3ab4127ab084364
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-01-28 02:55:57 +0100

    Test for send_pronto added.

--- miio/tests/test_chuangmi_ir.py
+++ miio/tests/test_chuangmi_ir.py
@@ -9,6 +9,13 @@ PROSONIC_POWER_ON = 'Z6VPAAUCAABgAgAAxQYAAOUIAACUEQAAqyIAADSeAABwdQEAAAAAAAA' \
                     'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABFEBAQEBAQEBAgICAgIC' \
                     'AgEBAgECAQEBAQIBAgECAgICBgNXA1cDUA'
 
+PROSONIC_POWER_ON_PROTON = '0000006C00220002015B00AD001600160016001600160016' \
+                           '001600160016001600160016001600160016001600160041' \
+                           '001600410016004100160041001600410016004100160041' \
+                           '001600160016001600160041001600160016004100160016' \
+                           '001600160016001600160016001600410016001600160041' \
+                           '001600160016004100160041001600410016004100160623' \
+                           '015B005700160E6E'
 
 class DummyChuangmiIr(DummyDevice, ChuangmiIr):
     def __init__(self, *args, **kwargs):
@@ -60,3 +67,11 @@ class TestChuangmiIr(TestCase):
     def test_play(self):
         assert self.device.play(PROSONIC_POWER_ON) is True
         assert self.device.play(PROSONIC_POWER_ON, 19200) is True
+
+    def test_play_pronto(self):
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON) is True
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON, 0) is True
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON, 1) is True
+
+        with pytest.raises(ChuangmiIrException):
+            self.device.play_pronto(PROSONIC_POWER_ON_PROTON, -1)
