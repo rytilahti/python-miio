6e483935341a446461fc9efb10cde21b0b3d96ec
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-01-29 07:36:50 +0100

    Minimum supported version of construct specified.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=["miio", "mirobo"],
 
     python_requires='>=3.4',
-    install_requires=['construct',
+    install_requires=['construct>=2.9.23',
                       'click',
                       'cryptography',
                       'pretty_cron',
