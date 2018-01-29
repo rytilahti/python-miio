27e52988168a18da1b86532d4b3b6faaf3877c32
Marcin Jaworski <marcin@jaworski.me>
2018-01-29 02:32:24 +0100

    Chuangmi IR: raw/pronto command type autodetection

--- miio/tests/test_chuangmi_ir.py
+++ miio/tests/test_chuangmi_ir.py
@@ -9,7 +9,7 @@ PROSONIC_POWER_ON = 'Z6VPAAUCAABgAgAAxQYAAOUIAACUEQAAqyIAADSeAABwdQEAAAAAAAA' \
                     'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABFEBAQEBAQEBAgICAgIC' \
                     'AgEBAgECAQEBAQIBAgECAgICBgNXA1cDUA'
 
-PROSONIC_POWER_ON_PROTON = '0000006C00220002015B00AD001600160016001600160016' \
+PROSONIC_POWER_ON_PRONTO = '0000006C00220002015B00AD001600160016001600160016' \
                            '001600160016001600160016001600160016001600160041' \
                            '001600410016004100160041001600410016004100160041' \
                            '001600160016001600160041001600160016004100160016' \
@@ -65,14 +65,14 @@ class TestChuangmiIr(TestCase):
         with pytest.raises(ChuangmiIrException):
             self.device.read(1000001)
 
-    def test_play(self):
-        assert self.device.play(PROSONIC_POWER_ON) is True
-        assert self.device.play(PROSONIC_POWER_ON, 19200) is True
+    def test_play_raw(self):
+        assert self.device.play_raw(PROSONIC_POWER_ON) is True
+        assert self.device.play_raw(PROSONIC_POWER_ON, 19200) is True
 
     def test_play_pronto(self):
-        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON) is True
-        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON, 0) is True
-        assert self.device.play_pronto(PROSONIC_POWER_ON_PROTON, 1) is True
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PRONTO) is True
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PRONTO, 0) is True
+        assert self.device.play_pronto(PROSONIC_POWER_ON_PRONTO, 1) is True
 
         with pytest.raises(ChuangmiIrException):
-            self.device.play_pronto(PROSONIC_POWER_ON_PROTON, -1)
+            self.device.play_pronto(PROSONIC_POWER_ON_PRONTO, -1)
