0128d89bb84e889fe19ba6060534cb1b80260fb3
Jens Schmer <jens.schmer+git@gmail.com>
2018-02-25 14:39:04 +0100

    Try to fix decrypted payload quirks if it fails to parse as json

--- miio/tests/test_protocol.py
+++ miio/tests/test_protocol.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from .. import Utils
+from ..protocol import Message
+import binascii
 
 
 class TestProtocol(TestCase):
@@ -17,7 +19,7 @@ class TestProtocol(TestCase):
 
         encrypted = Utils.encrypt(payload, token)
         decrypted = Utils.decrypt(encrypted, token)
-        self.assertEquals(payload, decrypted)
+        assert payload == decrypted
 
     def test_invalid_token(self):
         payload = b"hello world"
@@ -32,3 +34,45 @@ class TestProtocol(TestCase):
             Utils.encrypt(payload, wrong_length)
         with self.assertRaises(ValueError):
             Utils.decrypt(payload, wrong_length)
+
+    def test_decode_json_payload(self):
+        token = bytes.fromhex(32 * '0')
+        ctx = {'token': token}
+
+        def build_msg(data):
+            encrypted_data = Utils.encrypt(data, token)
+
+            # header
+            magic = binascii.unhexlify(b'2131')
+            length = (32 + len(encrypted_data)).to_bytes(2, byteorder='big')
+            unknown = binascii.unhexlify(b'00000000')
+            did = binascii.unhexlify(b'01234567')
+            epoch = binascii.unhexlify(b'00000000')
+
+            checksum = Utils.md5(magic+length+unknown+did+epoch+token+encrypted_data)
+
+            return magic+length+unknown+did+epoch+checksum+encrypted_data
+
+        # can parse message with valid json
+        serialized_msg = build_msg(b'{"id": 123456}')
+        parsed_msg = Message.parse(serialized_msg, **ctx)
+        assert parsed_msg.data.value
+        assert isinstance(parsed_msg.data.value, dict)
+        assert parsed_msg.data.value['id'] == 123456
+
+        # can parse message with invalid json for edge case powerstrip
+        # when not connected to cloud
+        serialized_msg = build_msg(b'{"id": 123456,,"otu_stat":0}')
+        parsed_msg = Message.parse(serialized_msg, **ctx)
+        assert parsed_msg.data.value
+        assert isinstance(parsed_msg.data.value, dict)
+        assert parsed_msg.data.value['id'] == 123456
+        assert parsed_msg.data.value['otu_stat'] == 0
+
+        # can parse message with invalid json for edge case xiaomi cloud
+        # reply to _sync.batch_gen_room_up_url
+        serialized_msg = build_msg(b'{"id": 123456}\x00k')
+        parsed_msg = Message.parse(serialized_msg, **ctx)
+        assert parsed_msg.data.value
+        assert isinstance(parsed_msg.data.value, dict)
+        assert parsed_msg.data.value['id'] == 123456
