70a25215b28d74701a8398ebef8ce58314dbe157
Sebastian Muszynski <s.muszynski@constructiva.de>
2018-03-31 21:21:22 +0200

    Add unified cli support to Fan

--- miio/fan.py
+++ miio/fan.py
@@ -2,6 +2,9 @@ import enum
 import logging
 from typing import Any, Dict, Optional
 
+import click
+
+from .click_common import command, format_output, EnumType
 from .device import Device
 
 _LOGGER = logging.getLogger(__name__)
@@ -116,21 +119,65 @@ class FanStatus:
         return self.data["angle"]
 
     def __str__(self) -> str:
-        s = "<FanStatus power=%s, temperature=%s, humidity=%s, led=%s, " \
-            "led_brightness=%s buzzer=%s, child_lock=%s, natural_level=%s, " \
-            "speed_level=%s, oscillate=%s, battery=%s, ac_power=%s, " \
-            "poweroff_time=%s, speed=%s, angle=%s" % \
-            (self.power, self.temperature, self.humidity, self.led,
-             self.led_brightness, self.buzzer, self.child_lock,
-             self.natural_level, self.speed_level, self.oscillate,
-             self.battery, self.ac_power, self.poweroff_time,
-             self.speed_level, self.angle)
+        s = "<FanStatus power=%s, " \
+            "temperature=%s, " \
+            "humidity=%s, " \
+            "led=%s, " \
+            "led_brightness=%s, " \
+            "buzzer=%s, " \
+            "child_lock=%s, " \
+            "natural_level=%s, " \
+            "speed_level=%s, " \
+            "oscillate=%s, " \
+            "battery=%s, " \
+            "ac_power=%s, " \
+            "poweroff_time=%s, " \
+            "speed=%s, " \
+            "angle=%s" % \
+            (self.power,
+             self.temperature,
+             self.humidity,
+             self.led,
+             self.led_brightness,
+             self.buzzer,
+             self.child_lock,
+             self.natural_level,
+             self.speed_level,
+             self.oscillate,
+             self.battery,
+             self.ac_power,
+             self.poweroff_time,
+             self.speed_level,
+             self.angle)
         return s
+
+    def __json__(self):
+        return self.data
 
 
 class Fan(Device):
     """Main class representing the Xiaomi Smart Fan."""
 
+    @command(
+        default_output=format_output(
+            "",
+            "Power: {result.power}\n"
+            "Battery: {result.battery} %\n"
+            "AC power: {result.ac_power}\n"
+            "Temperature: {result.temperature} °C\n"
+            "Humidity: {result.humidity} %\n"
+            "LED: {result.led}\n"
+            "LED brightness: {result.led_brightness}\n"
+            "Buzzer: {result.buzzer}\n"
+            "Child lock: {result.child_lock}\n"
+            "Natural level: {result.natural_level}\n"
+            "Speed level: {result.speed_level}\n"
+            "Oscillate: {result.oscillate}\n"
+            "Power-off time: {result.poweroff_time}\n"
+            "Speed: {result.speed}\n"
+            "Angle: {result.angle}\n"
+        )
+    )
     def status(self) -> FanStatus:
         """Retrieve properties."""
         properties = ['temp_dec', 'humidity', 'angle', 'speed',
@@ -153,56 +200,102 @@ class Fan(Device):
 
         return FanStatus(dict(zip(properties, values)))
 
+    @command(
+        default_output=format_output("Powering on"),
+    )
     def on(self):
         """Power on."""
         return self.send("set_power", ["on"])
 
+    @command(
+        default_output=format_output("Powering off"),
+    )
     def off(self):
         """Power off."""
         return self.send("set_power", ["off"])
 
+    @command(
+        click.argument("speed", type=int),
+        default_output=format_output("Setting natural level to {level}")
+    )
     def set_natural_level(self, level: int):
         """Set natural level."""
         level = max(0, min(level, 100))
         return self.send("set_natural_level", [level])  # 0...100
 
+    @command(
+        click.argument("speed", type=int),
+        default_output=format_output("Setting speed level to {level}")
+    )
     def set_speed_level(self, level: int):
         """Set speed level."""
         level = max(0, min(level, 100))
         return self.send("set_speed_level", [level])  # 0...100
 
+    @command(
+        click.argument("direction", type=EnumType(MoveDirection, False)),
+        default_output=format_output(
+            "Setting move direction to {direction}")
+    )
     def set_direction(self, direction: MoveDirection):
         """Set move direction."""
         return self.send("set_move", [direction.value])
 
+    @command(
+        click.argument("angle", type=int),
+        default_output=format_output("Setting angle to {angle}")
+    )
     def fan_set_angle(self, angle: int):
         """Set angle."""
         return self.send("set_angle", [angle])
 
+    @command(
+        default_output=format_output("Turning on oscillate"),
+    )
     def oscillate_on(self):
         """Enable oscillate."""
         return self.send("set_angle_enable", ["on"])
 
+    @command(
+        default_output=format_output("Turning off oscillate"),
+    )
     def oscillate_off(self):
         """Disable oscillate."""
         return self.send("set_angle_enable", ["off"])
 
+    @command(
+        click.argument("brightness", type=EnumType(LedBrightness, False)),
+        default_output=format_output(
+            "Setting LED brightness to {brightness}")
+    )
     def set_led_brightness(self, brightness: LedBrightness):
         """Set led brightness."""
         return self.send("set_led_b", [brightness.value])
 
+    @command(
+        default_output=format_output("Turning on LED"),
+    )
     def led_on(self):
         """Turn led on."""
         return self.send("set_led", ["on"])
 
+    @command(
+        default_output=format_output("Turning off LED"),
+    )
     def led_off(self):
         """Turn led off."""
         return self.send("set_led", ["off"])
 
+    @command(
+        default_output=format_output("Turning on buzzer"),
+    )
     def buzzer_on(self):
         """Enable buzzer."""
         return self.send("set_buzzer", ["on"])
 
+    @command(
+        default_output=format_output("Turning off buzzer"),
+    )
     def buzzer_off(self):
         """Disable buzzer."""
         return self.send("set_buzzer", ["off"])
