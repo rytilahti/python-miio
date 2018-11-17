2e40d0e3ad34ae37045ba99ef892cbde91d83623
Teemu R <tpr@iki.fi>
2018-11-17 07:53:42 +0100

    Add a missing parameter for pop to let last_clean_details to return the latest cleanup details (#413)

--- miio/vacuum.py
+++ miio/vacuum.py
@@ -191,7 +191,7 @@ class Vacuum(Device):
     @command()
     def last_clean_details(self) -> CleaningDetails:
         """Return details from the last cleaning."""
-        last_clean_id = self.clean_history().ids.pop()
+        last_clean_id = self.clean_history().ids.pop(0)
         return self.clean_details(last_clean_id, return_list=False)
 
     @command(
