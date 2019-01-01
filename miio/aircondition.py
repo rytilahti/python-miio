e92177afb1268d53216e5749ef13b371def500d0
Sebastian Muszynski <basti@linkt.de>
2019-01-01 21:39:30 +0100

    Add all setter

--- miio/aircondition.py
+++ miio/aircondition.py
@@ -3,7 +3,7 @@ import logging
 from collections import defaultdict
 from typing import Optional
 
-from .click_common import command, format_output
+from .click_common import command, format_output, EnumType
 from .device import Device, DeviceException
 
 _LOGGER = logging.getLogger(__name__)
@@ -53,7 +53,6 @@ class AirConditionStatus:
          'remote_mac': null,
          'htsensor_mac': null,
          'speed_level': 5,
-         'vertical_swing': 'on',
          'ht_sensor': null,
          'comfort': 'off',
          'ot_run_temp': 7,
@@ -140,9 +139,9 @@ class AirCondition(Device):
         properties = ['mode', 'lcd_auto', 'lcd_level', 'volume', 'idle_timer', 'open_timer',
                       'power', 'temp_dec', 'st_temp_dec', 'speed_level', 'vertical_swing',
                       'ptc', 'ptc_rt', 'silent', 'vertical_end', 'vertical_rt', 'speed_level',
-                      'vertical_swing', 'comfort', 'ot_run_temp', 'ep_temp', 'es_temp',
-                      'he_temp', 'compressor_frq', 'motor_speed', 'humidity', 'ele_quantity',
-                      'ex_humidity', 'remote_mac', 'htsensor_mac', 'ht_sensor', 'ot_humidity']
+                      'comfort', 'ot_run_temp', 'ep_temp', 'es_temp', 'he_temp', 'compressor_frq',
+                      'motor_speed', 'humidity', 'ele_quantity', 'ex_humidity', 'remote_mac',
+                      'htsensor_mac', 'ht_sensor', 'ot_humidity']
 
         # A single request is limited to 16 properties. Therefore the
         # properties are divided into multiple requests
@@ -176,3 +175,144 @@ class AirCondition(Device):
     def off(self):
         """Turn the air condition off."""
         return self.send("set_power", ["off"])
+
+    @command(
+        click.argument("temperature", type=float),
+        default_output=format_output(
+            "Setting target temperature to {temperature} degrees")
+    )
+    def set_temperature(self, temperature: float):
+        """Set target temperature."""
+        return self.send("set_temperature", [int(temperature * 10)])
+
+    @command(
+        click.argument("speed", type=int),
+        default_output=format_output(
+            "Setting speed to {speed}")
+    )
+    def set_speed(self, speed: int):
+        """Set speed."""
+        if speed < 0 or speed > 5:
+            raise AirConditionException("Invalid speed level: %s", speed)
+
+        return self.send("set_spd_level", [speed])
+
+    @command(
+        click.argument("start", type=int),
+        click.argument("stop", type=int),
+        default_output=format_output("Setting swing range from {start} degrees to {stop} degrees")
+    )
+    def set_swing_range(self, start: int=0, stop: int=60):
+        """Set swing range."""
+        if start < 0 or stop <= start:
+            raise AirConditionException("Invalid swing range: %s %s", start, stop)
+
+        return self.send("set_ver_range", [start, stop])
+
+    @command(
+        click.argument("swing", type=bool),
+        default_output=format_output(
+            lambda swing: "Turning on swing mode"
+            if swing else "Turning off swing mode"
+        )
+    )
+    def set_swing(self, swing: bool):
+        """Set swing on/off."""
+        if swing:
+            return self.send("set_vertical", ["on"])
+        else:
+            return self.send("set_vertical", ["off"])
+
+    @command(
+        click.argument("direction", type=int),
+        default_output=format_output(
+            "Setting wind direction to {direction} degree")
+    )
+    def set_wind_direction(self, direction: int):
+        """Set wind direction."""
+        return self.send("set_ver_pos", [direction])
+
+    @command(
+        click.argument("comfort", type=bool),
+        default_output=format_output(
+            lambda comfort: "Turning on comfort mode"
+            if comfort else "Turning off comfort mode"
+        )
+    )
+    def set_comfort(self, comfort: bool):
+        """Set comfort on/off."""
+        if comfort:
+            return self.send("set_comfort", ["on"])
+        else:
+            return self.send("set_comfort", ["off"])
+
+    @command(
+        click.argument("silent", type=bool),
+        default_output=format_output(
+            lambda silent: "Turning on silent mode"
+            if silent else "Turning off silent mode"
+        )
+    )
+    def set_silent(self, silent: bool):
+        """Set silent on/off."""
+        if silent:
+            return self.send("set_silent", ["on"])
+        else:
+            return self.send("set_silent", ["off"])
+
+    @command(
+        click.argument("ptc", type=bool),
+        default_output=format_output(
+            lambda ptc: "Turning on ptc mode"
+            if ptc else "Turning off ptc mode"
+        )
+    )
+    def set_ptc(self, ptc: bool):
+        """Set ptc on/off."""
+        if ptc:
+            return self.send("set_ptc", ["on"])
+        else:
+            return self.send("set_ptc", ["off"])
+
+    @command(
+        click.argument("volume", type=bool),
+        default_output=format_output(
+            lambda ptc: "Turning on ptc mode"
+            if ptc else "Turning off ptc mode"
+        )
+    )
+    def set_mute_audio(self, mute: bool):
+        """Mute audio on/off."""
+        if mute:
+            return self.send("set_volume_sw", ["off"])
+        else:
+            return self.send("set_volume_sw", ["on"])
+
+    @command(
+        click.argument("brightness", type=int),
+        default_output=format_output(
+            "Setting LCD brightness to {brightness}")
+    )
+    def set_lcd_brightness(self, brightness: int):
+        """Set lcd brightness."""
+        if brightness < 0 or brightness > 5:
+            raise AirConditionException("Invalid LCD brightness: %s", brightness)
+
+        return self.send("set_lcd", [brightness])
+
+    @command(
+        click.argument("mode", type=EnumType(OperationMode, False)),
+        default_output=format_output("Setting mode to '{mode.value}'")
+    )
+    def set_mode(self, mode: OperationMode):
+        """Set mode."""
+        return self.send("set_mode", [mode.value])
+
+    @command(
+        click.argument("seconds", type=int),
+        default_output=format_output(
+            "Setting idle timer to {seconds} seconds")
+    )
+    def set_idle_timer(self, seconds: int):
+        """Set idle timer."""
+        return self.send("set_idle_timer", [seconds])
