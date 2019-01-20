6b2ce4779dcf2cd76a24369fb900b1ea8914fcda
Dominique Barton <domibarton@users.noreply.github.com>
2019-01-20 14:23:37 +0100

    Sphinx config fix (#458)
    * Ignore .venv directory and docs _build directory in git
    
    We don't want those directories commited to the git repository.
    
    Python virtual environments are often created in a .venv/ directory,
    thus ignoring all .venv/ directories.
    
    The docs/_build directory is automatically created by Sphinx when the
    documentation is generated. We don't want the generated documentation
    to be commited to the repository.
    
    * Ignore .venv directory in Sphinx config
    
    This is required because if someone stores the virtualenv in a .venv
    directory (common practice) under the docs/ path, then Sphinx will try
    to pickup all files under the virtualenv. This leads to a lot of errors,
    performance impacts and side effects.
    
    * Remove static HTML path from Sphinx config
    
    There are currently no additional static files for Sphinx in place,
    but there's a _static directory configured in the Sphinx docs. When
    creating the Sphinx docs without an `mkdocs _static` command before,
    this leads to an error.
    
    Thus removing the _static path until we need it again.

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,12 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '.venv'
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -101,7 +106,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
