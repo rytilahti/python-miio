420c1c7e476c381f018705559a0a9e7fb370ec39
Teemu R <tpr@iki.fi>
2019-02-03 15:41:40 +0100

    Check for empty cleaning_history instead of crashing on it (#472)

--- miio/vacuum.py
+++ miio/vacuum.py
@@ -275,9 +275,15 @@ class Vacuum(Device):
         return CleaningSummary(self.send("get_clean_summary"))
 
     @command()
-    def last_clean_details(self) -> CleaningDetails:
-        """Return details from the last cleaning."""
-        last_clean_id = self.clean_history().ids.pop(0)
+    def last_clean_details(self) -> Optional[CleaningDetails]:
+        """Return details from the last cleaning.
+
+        Returns None if there has been no cleanups."""
+        history = self.clean_history()
+        if not history.ids:
+            return None
+
+        last_clean_id = history.ids.pop(0)
         return self.clean_details(last_clean_id, return_list=False)
 
     @command(
