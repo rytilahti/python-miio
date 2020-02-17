964de413aeecd7209267ee15c5ace969ab843621
starkillerOG <starkiller.og@gmail.com>
2020-02-17 10:42:38 +0100

    conform to black code style

--- miio/gateway_alarm.py
+++ miio/gateway_alarm.py
@@ -5,18 +5,18 @@ from .device import Device
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class XiaomiGatewayAlarm(Device):
     """Main class representing the Xiaomi Gateway Alarm."""
 
     def __init__(self, Device) -> None:
         self._device = Device
 
-    @command(
-        default_output=format_output("[alarm_status]"))
+    @command(default_output=format_output("[alarm_status]"))
     def status(self):
         """Fetch alarm status from the device."""
         # Response: ['on'], ['off'], ['oning']
-        alarm_status = self._device.send("get_arming") 
+        alarm_status = self._device.send("get_arming")
 
         return alarm_status
 
