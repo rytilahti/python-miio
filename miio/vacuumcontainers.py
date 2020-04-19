d97e4720e3660c1b67882cb20451a8d073c3c953
CkEsc <iamckesc@gmail.com>
2020-04-19 04:18:45 +0300

    Add property for water box (water tank) attach status

--- miio/vacuumcontainers.py
+++ miio/vacuumcontainers.py
@@ -168,6 +168,11 @@ class VacuumStatus:
         )
 
     @property
+    def is_water_box_attached(self) -> int:
+        """Return True is water box is installed."""
+        return self.data["water_box_status"] == 1
+
+    @property
     def got_error(self) -> bool:
         """True if an error has occured."""
         return self.error_code != 0
