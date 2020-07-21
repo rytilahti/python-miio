a6072e28e56a2306dee785dec441e0158610519d
starkillerOG <starkiller.og@gmail.com>
2020-07-21 21:26:17 +0200

    process revieuw comments

--- miio/gateway.py
+++ miio/gateway.py
@@ -647,69 +647,79 @@ class GatewayLight(GatewayDevice):
 
     @command()
     def rgb_status(self):
-        """Get current status of the light (always correct)."""
+        """
+        Get current status of the light.
+        Always represents the current status of the LEDs as oposed to while night_light_status.
+
+        Example:
+           {"is_on": false, "brightness": 0, "rgb": (0, 0, 0)}
+        """
         # Returns {"is_on": false, "brightness": 0, "rgb": (0, 0, 0)} when light is off
         state_int = self._gateway.send("get_rgb").pop()
         brightness = int_to_brightness(state_int)
         rgb = int_to_rgb(state_int)
-        if brightness > 0:
-            is_on = True
-        else:
-            is_on = False
+        is_on = brightness > 0
+
         return {"is_on": is_on, "brightness": brightness, "rgb": rgb}
 
     @command()
     def night_light_status(self):
-        """Get status of the night light."""
-        # The night light controls the same LEDs as the rgb light, but has a seprate memory for brightness and color.
-        # Changing the rgb light does not effect the stored state of the night light, while changing the night light does effect the state of the rgb light.
-        # Therefore this command only gives the correct status of the LEDs if the last command was a night light command and not a rgb light command, otherwise it gives the stored values of the night light.
-        # Returns {"is_on": false, "brightness": 0, "rgb": (0, 0, 0)} when night light is off
+        """
+        Get status of the night light.
+
+        The night light controls the same LEDs as the rgb light, but has a seprate memory for brightness and color.
+        Changing the rgb light does not effect the stored state of the night light, while changing the night light does effect the state of the rgb light.
+        Therefore this command only gives the correct status of the LEDs if the last command was a night light command and not a rgb light command, otherwise it gives the stored values of the night light.
+        Example:
+           {"is_on": false, "brightness": 0, "rgb": (0, 0, 0)}
+        """
         state_int = self._gateway.send("get_night_light_rgb").pop()
         brightness = int_to_brightness(state_int)
         rgb = int_to_rgb(state_int)
-        if brightness > 0:
-            is_on = True
-        else:
-            is_on = False
+        is_on = brightness > 0
+
         return {"is_on": is_on, "brightness": brightness, "rgb": rgb}
 
     @command(
         click.argument("brightness", type=int),
         click.argument("rgb", type=(int, int, int)),
     )
-    def set_rgb(self, brightness, rgb):
+    def set_rgb(self, brightness: int, rgb: Tuple[int, int, int]):
         """Set gateway light using brightness and rgb tuple."""
         brightness_and_color = brightness_and_color_to_int(brightness, rgb)
+
         return self._gateway.send("set_rgb", [brightness_and_color])
 
     @command(
         click.argument("brightness", type=int),
         click.argument("rgb", type=(int, int, int)),
     )
-    def set_night_light(self, brightness, rgb):
+    def set_night_light(self, brightness: int, rgb: Tuple[int, int, int]):
         """Set gateway night light using brightness and rgb tuple."""
         brightness_and_color = brightness_and_color_to_int(brightness, rgb)
+
         return self._gateway.send("set_night_light_rgb", [brightness_and_color])
 
     @command(click.argument("brightness", type=int))
-    def set_rgb_brightness(self, brightness):
+    def set_rgb_brightness(self, brightness: int):
         """Set gateway light brightness (0-100)."""
         if 100 < brightness < 0:
             raise Exception("Brightness must be between 0 and 100")
         current_color = self.rgb_status()["rgb"]
+
         return self.set_rgb(brightness, current_color)
 
     @command(click.argument("brightness", type=int))
-    def set_night_light_brightness(self, brightness):
+    def set_night_light_brightness(self, brightness: int):
         """Set night light brightness (0-100)."""
         if 100 < brightness < 0:
             raise Exception("Brightness must be between 0 and 100")
         current_color = self.night_light_status()["rgb"]
+
         return self.set_night_light(brightness, current_color)
 
     @command(click.argument("color_name", type=str))
-    def set_rgb_color(self, color_name):
+    def set_rgb_color(self, color_name: str):
         """Set gateway light color using color name (red, green, etc)."""
         if color_name not in color_map.keys():
             raise Exception(
@@ -718,10 +728,11 @@ class GatewayLight(GatewayDevice):
                 )
             )
         current_brightness = self.rgb_status()["brightness"]
+
         return self.set_rgb(current_brightness, color_map[color_name])
 
     @command(click.argument("color_name", type=str))
-    def set_night_light_color(self, color_name):
+    def set_night_light_color(self, color_name: str):
         """Set night light color using color name (red, green, etc)."""
         if color_name not in color_map.keys():
             raise Exception(
@@ -730,12 +741,13 @@ class GatewayLight(GatewayDevice):
                 )
             )
         current_brightness = self.night_light_status()["brightness"]
+
         return self.set_night_light(current_brightness, color_map[color_name])
 
     @command(
         click.argument("color_name", type=str), click.argument("brightness", type=int),
     )
-    def set_rgb_using_name(self, color_name, brightness):
+    def set_rgb_using_name(self, color_name: str, brightness: int):
         """Set gateway light color (using color name) and brightness (0-100)."""
         if 100 < brightness < 0:
             raise Exception("Brightness must be between 0 and 100")
@@ -745,12 +757,13 @@ class GatewayLight(GatewayDevice):
                     color=color_name, colors=color_map.keys()
                 )
             )
+
         return self.set_rgb(brightness, color_map[color_name])
 
     @command(
         click.argument("color_name", type=str), click.argument("brightness", type=int),
     )
-    def set_night_light_using_name(self, color_name, brightness):
+    def set_night_light_using_name(self, color_name: str, brightness: int):
         """Set night light color (using color name) and brightness (0-100)."""
         if 100 < brightness < 0:
             raise Exception("Brightness must be between 0 and 100")
@@ -760,6 +773,7 @@ class GatewayLight(GatewayDevice):
                     color=color_name, colors=color_map.keys()
                 )
             )
+
         return self.set_night_light(brightness, color_map[color_name])
 
 
