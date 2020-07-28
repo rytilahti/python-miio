d823e22e6b609aa2e85b6254f6ce42d3069ccad6
Teemu R <tpr@iki.fi>
2020-07-28 18:47:51 +0200

    Improve documentation presentation (#777)
    * Improve documentation presentation
    
    * Use rtd theme, which is much more readable than alabaster
    * Show inherited members from the base classes
    * Order elements by type instead by alphabetical order
    
    * update poetry.lock to include the theme
    
    * decomment the groupwise ordering

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -188,3 +188,6 @@ apidoc_module_dir = "../miio"
 apidoc_output_dir = "api"
 apidoc_excluded_paths = ["tests"]
 apidoc_separate_modules = True
+autodoc_member_order = "groupwise"
+autodoc_inherit_docstrings = True
+autodoc_default_options = {"inherited-members": True}
