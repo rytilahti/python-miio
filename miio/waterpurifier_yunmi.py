fd656df14df0235570b02d526cce15e0e65cda16
zhangjingye03 <934526987@qq.com>
2020-10-29 12:47:47 +0800

    Use timedelta as hours, add remaining indicator and error code parser

--- miio/waterpurifier_yunmi.py
+++ miio/waterpurifier_yunmi.py
@@ -1,11 +1,105 @@
 import logging
 from datetime import timedelta
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from .click_common import command, format_output
 from .device import Device
 
 _LOGGER = logging.getLogger(__name__)
+
+ERROR_DESCRIPTION = [
+    {
+        "name": "Water temperature anomaly",
+        "advice": "Check if inlet water temperature is among 5~38℃.",
+    },
+    {
+        "name": "Inlet water flow meter damaged",
+        "advice": "Try to purify water again after reinstalling the filter for serval times.",
+    },
+    {
+        "name": "Water flow sensor anomaly",
+        "advice": "Check if the water pressure is too low.",
+    },
+    {"name": "Filter life expired", "advice": "Replace filter."},
+    {"name": "WiFi communication error", "advice": "Contact the after-sales."},
+    {"name": "EEPROM communication error", "advice": "Contact the after-sales."},
+    {"name": "RFID communication error", "advice": "Contact the after-sales."},
+    {
+        "name": "Faucet communication error",
+        "advice": "Try to plug in the faucet again.",
+    },
+    {
+        "name": "Purified water flow sensor anomaly",
+        "advice": "Check whether all filters are properly installed and water pressure is normal.",
+    },
+    {
+        "name": "Water leak",
+        "advice": "Check if there is water leaking around the water purifier.",
+    },
+    {"name": "Floater anomaly", "advice": "Contact the after-sales."},
+    {"name": "TDS anomaly", "advice": "Check if the RO filter is expired."},
+    {
+        "name": "Water temperature too high",
+        "advice": "Check if inlet water is warm water with temperature above 40℃.",
+    },
+    {
+        "name": "Recovery rate anomaly",
+        "advice": "Check if the waste water pipe works abnormally and the RO filter is expired.",
+    },
+    {
+        "name": "Outlet water quality anomaly",
+        "advice": "Check if the waste water pipe works abnormally and the RO filter is expired.",
+    },
+    {
+        "name": "Thermal protection for pumps",
+        "advice": "The water purifier has worked for a long time, please use it after 20 minutes.",
+    },
+    {
+        "name": "Dry burning protection",
+        "advice": "Check if the inlet water pipe works abnormally.",
+    },
+    {
+        "name": "Outlet water NTC anomaly",
+        "advice": "Switch off the purifier and restart it again.",
+    },
+    {
+        "name": "Dry burning NTC anomaly",
+        "advice": "Switch off the purifier and restart it again.",
+    },
+    {
+        "name": "Heater anomaly",
+        "advice": "Switch off the purifier and restart it again.",
+    },
+]
+
+
+class RunStatus:
+    def __init__(self, run_status: int):
+        """
+        Running status parser.
+
+        Return value of run_status: <int>
+
+        We should convert the run_status code to binary, each bit from
+        LSB to MSB represents one error. It's able to cover multiple errors.
+
+        Example run_status value: 9 (binary: 1001)
+        Thus, the purifier reports 2 errors, stands bit 0 and bit 3,
+        means "Water temperature anomaly" and "Filter life expired".
+        """
+        self.err_list = [
+            ERROR_DESCRIPTION[i]
+            for i in range(0, len(ERROR_DESCRIPTION))
+            if (1 << i) & run_status
+        ]
+
+    @property
+    def errors(self) -> List:
+        return self.err_list
+
+    def __repr__(self) -> str:
+        s = "<RunStatus errors=%s>" % (self.errors)
+        return s
 
 
 class WaterPurifierYunmiStatus:
@@ -28,19 +122,24 @@ class WaterPurifierYunmiStatus:
         self.data = data
 
     @property
-    def status(self) -> int:
+    def error(self) -> RunStatus:
         """Current running status, 0 for no error."""
-        return self.data["run_status"]
+        return RunStatus(self.data["run_status"])
 
     @property
     def filter1_life_total(self) -> timedelta:
         """Filter1 total available time in hours."""
-        return self.data["f1_totaltime"]
+        return timedelta(hours=self.data["f1_totaltime"])
 
     @property
     def filter1_life_used(self) -> timedelta:
         """Filter1 used time in hours."""
-        return self.data["f1_usedtime"]
+        return timedelta(hours=self.data["f1_usedtime"])
+
+    @property
+    def filter1_life_remaining(self) -> timedelta:
+        """Filter1 remaining time in hours."""
+        return self.filter1_life_total - self.filter1_life_used
 
     @property
     def filter1_flow_total(self) -> int:
@@ -53,14 +152,24 @@ class WaterPurifierYunmiStatus:
         return self.data["f1_usedflow"]
 
     @property
+    def filter1_flow_remaining(self) -> timedelta:
+        """Filter1 remaining flow in Metric Liter (L)."""
+        return self.filter1_flow_total - self.filter1_flow_used
+
+    @property
     def filter2_life_total(self) -> timedelta:
         """Filter2 total available time in hours."""
-        return self.data["f2_totaltime"]
+        return timedelta(hours=self.data["f2_totaltime"])
 
     @property
     def filter2_life_used(self) -> timedelta:
         """Filter2 used time in hours."""
-        return self.data["f2_usedtime"]
+        return timedelta(hours=self.data["f2_usedtime"])
+
+    @property
+    def filter2_life_remaining(self) -> timedelta:
+        """Filter2 remaining time in hours."""
+        return self.filter2_life_total - self.filter2_life_used
 
     @property
     def filter2_flow_total(self) -> int:
@@ -73,14 +182,24 @@ class WaterPurifierYunmiStatus:
         return self.data["f2_usedflow"]
 
     @property
+    def filter2_flow_remaining(self) -> timedelta:
+        """Filter2 remaining flow in Metric Liter (L)."""
+        return self.filter2_flow_total - self.filter2_flow_used
+
+    @property
     def filter3_life_total(self) -> timedelta:
         """Filter3 total available time in hours."""
-        return self.data["f3_totaltime"]
+        return timedelta(hours=self.data["f3_totaltime"])
 
     @property
     def filter3_life_used(self) -> timedelta:
         """Filter3 used time in hours."""
-        return self.data["f3_usedtime"]
+        return timedelta(hours=self.data["f3_usedtime"])
+
+    @property
+    def filter3_life_remaining(self) -> timedelta:
+        """Filter3 remaining time in hours."""
+        return self.filter3_life_total - self.filter3_life_used
 
     @property
     def filter3_flow_total(self) -> int:
@@ -91,6 +210,11 @@ class WaterPurifierYunmiStatus:
     def filter3_flow_used(self) -> int:
         """Filter3 used flow in Metric Liter (L)."""
         return self.data["f3_usedflow"]
+
+    @property
+    def filter3_flow_remaining(self) -> timedelta:
+        """Filter1 remaining flow in Metric Liter (L)."""
+        return self.filter3_flow_total - self.filter3_flow_used
 
     @property
     def tds_in(self) -> int:
@@ -120,38 +244,50 @@ class WaterPurifierYunmiStatus:
     def __repr__(self) -> str:
         return (
             "<WaterPurifierYunmiStatus "
-            "status=%s, "
+            "error=%s, "
             "filter1_life_total=%s, "
             "filter1_life_used=%s, "
+            "filter1_life_remaining=%s, "
             "filter1_flow_total=%s, "
             "filter1_flow_used=%s, "
+            "filter1_flow_remaining=%s, "
             "filter2_life_total=%s, "
             "filter2_life_used=%s, "
+            "filter2_life_remaining=%s, "
             "filter2_flow_total=%s, "
             "filter2_flow_used=%s, "
+            "filter2_flow_remaining=%s, "
             "filter3_life_total=%s, "
             "filter3_life_used=%s, "
+            "filter3_life_remaining=%s, "
             "filter3_flow_total=%s, "
             "filter3_flow_used=%s, "
+            "filter3_flow_remaining=%s, "
             "tds_in=%s, "
             "tds_out=%s, "
             "rinse=%s, "
             "temperature=%s, "
             "tds_warn_thd=%s>"
             % (
-                self.status,
+                self.error,
                 self.filter1_life_total,
                 self.filter1_life_used,
+                self.filter1_life_remaining,
                 self.filter1_flow_total,
                 self.filter1_flow_used,
+                self.filter1_flow_remaining,
                 self.filter2_life_total,
                 self.filter2_life_used,
+                self.filter2_life_remaining,
                 self.filter2_flow_total,
                 self.filter2_flow_used,
+                self.filter2_flow_remaining,
                 self.filter3_life_total,
                 self.filter3_life_used,
+                self.filter3_life_remaining,
                 self.filter3_flow_total,
                 self.filter3_flow_used,
+                self.filter3_flow_remaining,
                 self.tds_in,
                 self.tds_out,
                 self.rinse,
@@ -170,23 +306,29 @@ class WaterPurifierYunmi(Device):
     @command(
         default_output=format_output(
             "",
-            "Error: {result.status}\n"
-            "Filter1 total time: {result.filter1_life_total} hours\n"
-            "Filter1 used time: {result.filter1_life_used} hours\n"
+            "Error: {result.error}\n"
+            "Filter1 total time: {result.filter1_life_total}\n"
+            "Filter1 used time: {result.filter1_life_used}\n"
+            "Filter1 remaining time: {result.filter1_life_remaining}\n"
             "Filter1 total flow: {result.filter1_flow_total} L\n"
             "Filter1 used flow: {result.filter1_flow_used} L\n"
-            "Filter2 total time: {result.filter2_life_total} hours\n"
-            "Filter2 used time: {result.filter2_life_used} hours\n"
+            "Filter1 remaining flow: {result.filter1_flow_remaining} L\n"
+            "Filter2 total time: {result.filter2_life_total}\n"
+            "Filter2 used time: {result.filter2_life_used}\n"
+            "Filter2 remaining time: {result.filter2_life_remaining}\n"
             "Filter2 total flow: {result.filter2_flow_total} L\n"
             "Filter2 used flow: {result.filter2_flow_used} L\n"
-            "Filter3 total time: {result.filter3_life_total} hours\n"
-            "Filter3 used time: {result.filter3_life_used} hours\n"
+            "Filter2 remaining flow: {result.filter2_flow_remaining} L\n"
+            "Filter3 total time: {result.filter3_life_total}\n"
+            "Filter3 used time: {result.filter3_life_used}\n"
+            "Filter3 remaining time: {result.filter3_life_remaining}\n"
             "Filter3 total flow: {result.filter3_flow_total} L\n"
             "Filter3 used flow: {result.filter3_flow_used} L\n"
+            "Filter3 remaining flow: {result.filter3_flow_remaining} L\n"
             "TDS in: {result.tds_in}\n"
             "TDS out: {result.tds_out}\n"
             "Rinsing: {result.rinse}\n"
-            "Temperature: {result.temperature}\n"
+            "Temperature: {result.temperature} ℃\n"
             "TDS warning threshold: {result.tds_warn_thd}\n",
         )
     )
