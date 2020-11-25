451a65a4a655cd811499cc8255e8dfb81b32ccc4
Alexey Priymak <alexey.priymak@dentsplysirona.com>
2020-11-25 22:59:50 +0200

    Added only example of JSON payload from the lamp

--- miio/huizuo.py
+++ miio/huizuo.py
@@ -27,47 +27,26 @@ class HuizuoException(DeviceException):
 
 class HuizuoStatus:
     def __init__(self, data):
-        # power = '{"siid":2,"piid":1}' values = true,false
-        # brightless = '{"siid":2,"piid":2}' values = 1-100
-        # color-temperature = '{"siid":2,"piid":3}' values = 3000-6400
+        """
+        Response of a Huizuo Pisces For Bedroom (huayi.light.pis123)
+         {'id': 1, 'result': [
+             {'did': '', 'siid': 2, 'piid': 1, 'code': 0, 'value': False},
+             {'did': '', 'siid': 2, 'piid': 2, 'code': 0, 'value': 94},
+             {'did': '', 'siid': 2, 'piid': 3, 'code': 0, 'value': 6400}
+             ]
+        }
 
-        ####################################################################################################################
-        # Complete response payload (miiocli -d):
-        # pi@raspberrypi:~ $ miiocli -d huizuo --ip 192.168.X.X --token ****** status
-        # INFO:miio.cli:Debug mode active
-
-        # DEBUG:miio.protocol:Unable to decrypt, returning raw bytes: b''
-        # DEBUG:miio.miioprotocol:Got a response: Container:
-        # data = Container:
-        #   data = b'' (total 0)
-        #   value = b'' (total 0)
-        #   offset1 = 32
-        #   offset2 = 32
-        #   length = 0
-        # header = Container:
-        #   data = b'!1\x00 \x00\x00\x00\x00\x12\xb5\n\xa3\x00\x045\xbc' (total 16)
-        #   value = Container:
-        #       length = 32
-        #       unknown = 0
-        #       device_id = unhexlify('12b50aa3')
-        #       ts = 1970-01-04 04:38:20
-        #   offset1 = 0
-        #   offset2 = 16
-        #   length = 16
-        # checksum = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff' (total 16)
-        # DEBUG:miio.miioprotocol:Discovered 12b50aa3 with ts: 1970-01-04 04:38:20, token: b'ffffffffffffffffffffffffffffffff'
-        # DEBUG:miio.miioprotocol:192.168.X.X:54321 >>: {'id': 1, 'method': 'get_prop', 'params': [{'siid': 2, 'piid': 1}, {'siid': 2, 'piid': 2}, {'siid': 2, 'piid': 3}]}
-        # DEBUG:miio.miioprotocol:192.168.X.X:54321 (ts: 1970-01-04 04:38:20, id: 1) << {'id': 1, 'result': [{'did': '', 'siid': 2, 'piid': 1, 'code': 0, 'value': False}, {'did': '', 'siid': 2, 'piid': 2, 'code': 0, 'value': 94}, {'did': '', 'siid': 2, 'piid': 3, 'code': 0, 'value': 6400}]}
-        # Power: False
-        # Brightness: 94
-        # Temperature: 6400
-        #########################################################################################################################
+        Explanation (line-by-line):
+            power = '{"siid":2,"piid":1}' values = true,false
+            brightless(%) = '{"siid":2,"piid":2}' values = 1-100
+            color temperature(Kelvin) = '{"siid":2,"piid":3}' values = 3000-6400
+        """
 
         self.data = data
 
     @property
     def is_on(self) -> bool:
-        """Return whether the bulb is on or off."""
+        """Return whether the lamp is on or off."""
         return self.data["power"]
 
     @property
