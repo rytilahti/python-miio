4b37810c05fdf08d8255c0dc424e933a018ee7f9
alexey_priymak <alexey.priymak@dentsplysirona.com>
2020-12-01 15:49:57 +0200

    Update mapping and status output

--- miio/huizuo.py
+++ miio/huizuo.py
@@ -43,7 +43,7 @@ MODEL_HUIZUO_WY203 = "huayi.light.wy203"
 # Lights: basic + heater
 MODEL_HUIZUO_WYHEAT = "huayi.light.wyheat"
 
-MODELS_SUPPORTED = [
+BASIC_MODELS = [
     MODEL_HUIZUO_PIS123,
     MODEL_HUIZUO_ARI013,
     MODEL_HUIZUO_ARIES,
@@ -55,37 +55,54 @@ MODELS_SUPPORTED = [
     MODEL_HUIZUO_VIR063,
     MODEL_HUIZUO_VIRGO,
     MODEL_HUIZUO_WY,
-    MODEL_HUIZUO_ZW131
-    ]
+    MODEL_HUIZUO_ZW131,
+]
 
+MODELS_WITH_FAN_WY = [MODEL_HUIZUO_FANWY]
+MODELS_WITH_FAN_WY2 = [MODEL_HUIZUO_FANWY2]
 
+MODELS_WITH_SCENES = [
+    MODEL_HUIZUO_WY200,
+    MODEL_HUIZUO_WY201,
+    MODEL_HUIZUO_WY202,
+    MODEL_HUIZUO_WY203,
+]
+
+MODELS_WITH_HEATER = [MODEL_HUIZUO_WYHEAT]
+
+MODELS_SUPPORTED = BASIC_MODELS
+
+# Define a basic mapping for properties, which exists for all lights
 _MAPPING = {
-    "power": {"siid": 2, "piid": 1}, # Boolean: True, False
-    "brightness": {"siid": 2, "piid": 2}, # Percentage: 1-100
-    "color_temp": {"siid": 2, "piid": 3}, # Kelvin: 3000-6400 (but for MODEL_HUIZUO_FANWY2: 3000-5700!)
+    "power": {"siid": 2, "piid": 1},  # Boolean: True, False
+    "brightness": {"siid": 2, "piid": 2},  # Percentage: 1-100
+    "color_temp": {
+        "siid": 2,
+        "piid": 3,
+    },  # Kelvin: 3000-6400 (but for MODEL_HUIZUO_FANWY2: 3000-5700!)
 }
 
-_MAPPING_FAN_WY2 = { # for MODEL_HUIZUO_FANWY2
-    "fan_power": {"siid": 3, "piid": 1}, # Boolean: True, False
-    "fan_level": {"siid": 3, "piid": 2}, # Percentage: 1-100
-    "fan_mode": {"siid": 3, "piid": 3}, # Enum: 0 - Basic, 1 - Natural wind
+_ADDITIONAL_MAPPING_FAN_WY2 = {  # for MODEL_HUIZUO_FANWY2
+    "fan_power": {"siid": 3, "piid": 1},  # Boolean: True, False
+    "fan_level": {"siid": 3, "piid": 2},  # Percentage: 1-100
+    "fan_mode": {"siid": 3, "piid": 3},  # Enum: 0 - Basic, 1 - Natural wind
 }
 
-_MAPPING_FAN_WY = { # for MODEL_HUIZUO_FANWY
-    "fan_power": {"siid": 3, "piid": 1}, # Boolean: True, False
-    "fan_level": {"siid": 3, "piid": 2}, # Percentage: 1-100
-    "fan_motor_reverse": {"siid": 3, "piid": 3}, # Boolean: True, False
-    "fan_mode": {"siid": 3, "piid": 4}, # Enum: 0 - Basic, 1 - Natural wind
+_ADDITIONAL_MAPPING_FAN_WY = {  # for MODEL_HUIZUO_FANWY
+    "fan_power": {"siid": 3, "piid": 1},  # Boolean: True, False
+    "fan_level": {"siid": 3, "piid": 2},  # Percentage: 1-100
+    "fan_motor_reverse": {"siid": 3, "piid": 3},  # Boolean: True, False
+    "fan_mode": {"siid": 3, "piid": 4},  # Enum: 0 - Basic, 1 - Natural wind
 }
 
-_MAPPING_HEATER = {
-    "heater_power": {"siid": 3, "piid": 1}, # Boolean: True, False
-    "heater_fault_code": {"siid": 3, "piid": 1}, # Fault code: 0 means "No fault"
-    "heat_level": {"siid": 3, "piid": 1}, # Enum: 1-3
+_ADDITIONAL_MAPPING_HEATER = {
+    "heater_power": {"siid": 3, "piid": 1},  # Boolean: True, False
+    "heater_fault_code": {"siid": 3, "piid": 1},  # Fault code: 0 means "No fault"
+    "heat_level": {"siid": 3, "piid": 1},  # Enum: 1-3
 }
 
-_MAPPING_SCENE = { # Only for write, send "0" to activate
-    "on_off": {"siid": 3, "piid": 1}, 
+_ADDITIONAL_MAPPING_SCENE = {  # Only for write, send "0" to activate
+    "on_off": {"siid": 3, "piid": 1},
     "brightness_add": {"siid": 3, "piid": 2},
     "brightness_dec": {"siid": 3, "piid": 3},
     "brightness_switch": {"siid": 3, "piid": 4},
@@ -93,30 +110,9 @@ _MAPPING_SCENE = { # Only for write, send "0" to activate
     "colortemp_dec": {"siid": 3, "piid": 6},
     "colortemp_switch": {"siid": 3, "piid": 7},
     "on_or_brightness": {"siid": 3, "piid": 8},
-    "on_or_colortemp":  {"siid": 3, "piid": 9},
+    "on_or_colortemp": {"siid": 3, "piid": 9},
 }
 
-_AVAILABLE_MAPPING = {
-    MODEL_HUIZUO_PIS123: _MAPPING,
-    MODEL_HUIZUO_ARI013: _MAPPING,
-    MODEL_HUIZUO_ARIES: _MAPPING,
-    MODEL_HUIZUO_PEG091: _MAPPING,
-    MODEL_HUIZUO_PEG093: _MAPPING,
-    MODEL_HUIZUO_PISCES: _MAPPING,
-    MODEL_HUIZUO_TAU023: _MAPPING,
-    MODEL_HUIZUO_TAURUS: _MAPPING,
-    MODEL_HUIZUO_VIR063: _MAPPING,
-    MODEL_HUIZUO_VIRGO: _MAPPING,
-    MODEL_HUIZUO_WY: _MAPPING,
-    MODEL_HUIZUO_ZW131: _MAPPING,
-    MODEL_HUIZUO_FANWY: _MAPPING.update(_MAPPING_FAN_WY),
-    MODEL_HUIZUO_FANWY2: _MAPPING.update(_MAPPING_FAN_WY2),
-    MODEL_HUIZUO_WY200: _MAPPING.update(_MAPPING_SCENE),
-    MODEL_HUIZUO_WY201: _MAPPING.update(_MAPPING_SCENE),
-    MODEL_HUIZUO_WY202: _MAPPING.update(_MAPPING_SCENE),
-    MODEL_HUIZUO_WY203: _MAPPING.update(_MAPPING_SCENE),
-    MODEL_HUIZUO_WYHEAT: _MAPPING.update(_MAPPING_SCENE)
-}
 
 
 class HuizuoException(DeviceException):
@@ -125,37 +121,28 @@ class HuizuoException(DeviceException):
 
 class HuizuoStatus:
     def __init__(self, data: Dict[str, Any]) -> None:
-        """
-        Response of a Huizuo Pisces For Bedroom (huayi.light.pis123)
-         {'id': 1, 'result': [
-             {'did': '', 'siid': 2, 'piid': 1, 'code': 0, 'value': False},
-             {'did': '', 'siid': 2, 'piid': 2, 'code': 0, 'value': 94},
-             {'did': '', 'siid': 2, 'piid': 3, 'code': 0, 'value': 6400}
-             ]
-        }
-
-        Explanation (line-by-line):
-            power = '{"siid":2,"piid":1}' values = true,false
-            brightless(%) = '{"siid":2,"piid":2}' values = 1-100
-            color temperature(Kelvin) = '{"siid":2,"piid":3}' values = 3000-6400
-        """
-
         self.data = data
 
     @property
     def is_on(self) -> bool:
         """Return True if device is on."""
-        return self.data["power"]
+        if "power" in self.data:
+            return self.data["power"]
+        return None
 
     @property
     def brightness(self) -> int:
         """Return current brightness."""
-        return self.data["brightness"]
+        if "brightness" in self.data:
+            return self.data["brightness"]
+        return None
 
     @property
     def color_temp(self) -> int:
         """Return current color temperature."""
-        return self.data["color_temp"]
+        if "color_temp" in self.data:
+            return self.data["color_temp"]
+        return None
 
     @property
     def is_fan_on(self) -> bool:
@@ -170,21 +157,21 @@ class HuizuoStatus:
         if "fan_level" in self.data:
             return self.data["fan_level"]
         return None
-    
+
     @property
     def is_fan_reverse(self) -> bool:
         """Return True if Fan reverse is on."""
         if "fan_motor_reverse" in self.data:
             return self.data["fan_motor_reverse"]
         return None
-    
+
     @property
     def fan_mode(self) -> int:
         """Return 0 if 'Basic' and 1 if 'Natural wind'"""
         if "fan_mode" in self.data:
             return self.data["fan_mode"]
         return None
-    
+
     @property
     def is_heater_on(self):
         """Return True if Heater is on."""
@@ -198,7 +185,7 @@ class HuizuoStatus:
         if "heater_fault_code" in self.data:
             return self.data["heater_fault_code"]
         return None
-    
+
     @property
     def heat_level(self):
         """Return Heater's heat level"""
@@ -208,17 +195,27 @@ class HuizuoStatus:
 
     def __repr__(self):
         parameters = []
-        if self.is_on is not None: parameters.append("on="+self.is_on) 
-        if self.brightness is not None: parameters.append("brightness="+self.brightness)
-        if self.color_temp is not None: parameters.append("color_temp="+self.color_temp)
-        if self.is_fan_on is not None: parameters.append("fan_on="+self.is_fan_on)
-        if self.fan_speed_level is not None: parameters.append("fan_level="+self.fan_speed_level)
-        if self.fan_mode is not None: parameters.append("fan_mode="+self.fan_mode)
-        if self.is_fan_reverse is not None: parameters.append("fan_motor_reverse="+self.is_fan_reverse)
-        if self.is_heater_on is not None: parameters.append("heater_on="+self.is_heater_on)
-        if self.heat_level is not None: parameters.append("heat_level="+self.heat_level)
-        if self.heater_fault_code is not None: parameters.append("heater_fault_code="+self.heater_fault_code)
-    
+        if self.is_on is not None:
+            parameters.append("on=" + self.is_on)
+        if self.brightness is not None:
+            parameters.append("brightness=" + self.brightness)
+        if self.color_temp is not None:
+            parameters.append("color_temp=" + self.color_temp)
+        if self.is_fan_on is not None:
+            parameters.append("fan_on=" + self.is_fan_on)
+        if self.fan_speed_level is not None:
+            parameters.append("fan_level=" + self.fan_speed_level)
+        if self.fan_mode is not None:
+            parameters.append("fan_mode=" + self.fan_mode)
+        if self.is_fan_reverse is not None:
+            parameters.append("fan_motor_reverse=" + self.is_fan_reverse)
+        if self.is_heater_on is not None:
+            parameters.append("heater_on=" + self.is_heater_on)
+        if self.heat_level is not None:
+            parameters.append("heat_level=" + self.heat_level)
+        if self.heater_fault_code is not None:
+            parameters.append("heater_fault_code=" + self.heater_fault_code)
+
         s = "<Huizuo " + " ".join(parameters) + ">"
         return s
 
@@ -235,15 +232,23 @@ class Huizuo(MiotDevice):
         lazy_discover: bool = True,
         model: str = MODEL_HUIZUO_PIS123,
     ) -> None:
+
+        if self.model in MODELS_WITH_FAN_WY:
+            _MAPPING.update(_ADDITIONAL_MAPPING_FAN_WY)
+        if self.model in MODELS_WITH_FAN_WY2:
+            _MAPPING.update(_ADDITIONAL_MAPPING_FAN_WY2)
+        if self.model in MODELS_WITH_SCENES:
+            _MAPPING.update(_ADDITIONAL_MAPPING_SCENE)
+        if self.model in MODELS_WITH_HEATER:
+            _MAPPING.update(_ADDITIONAL_MAPPING_HEATER)
+
         super().__init__(_MAPPING, ip, token, start_id, debug, lazy_discover)
 
         if model in MODELS_SUPPORTED:
             self.model = model
         else:
             self.model = MODEL_HUIZUO_PIS123
-            _LOGGER.error(
-                "Device model %s unsupported. Falling back to %s.", model, self.model
-            )
+            _LOGGER.error("Device model %s unsupported. Falling back to %s.", model, self.model)
 
     @command(
         default_output=format_output("Powering on"),
@@ -262,10 +267,21 @@ class Huizuo(MiotDevice):
     @command(
         default_output=format_output(
             "\n",
+            "------------ Basic parameters for lamp -----------\n",
             "Power: {result.is_on}\n"
             "Brightness: {result.brightness}\n"
             "Color Temperature: {result.color_temp}\n"
             "\n",
+            "--------- Parameters for models with fan ---------\n",
+            "Fan power:  {result.is_fan_on}\n",
+            "Fan level: {result.fan_speed_level}\n",
+            "Fan mode: {result.fan_mode}\n",
+            "Fan reverse: {result.fan_reverse}\n",
+            "\n",
+            "------- Parameters for models with heater --------\n",
+            "Heater power: {result.is_heater_on}\n",
+            "Heat level: {result.heat_level}\n",
+            "Heat fault code (0 means 'OK'): {result.heater_fault_code}\n",
         )
     )
     def status(self) -> HuizuoStatus:
@@ -295,7 +311,15 @@ class Huizuo(MiotDevice):
     )
     def set_color_temp(self, color_temp):
         """Set color temp in kelvin."""
-        if color_temp < 3000 or color_temp > 6400:
+
+        # I don't know why only one lamp has smaller color temperature (based on specs),
+        # but let's process it correctly
+        if self.model == MODELS_WITH_FAN_WY2:
+            max_color_temp = 5700
+        else:
+            max_color_temp = 6400
+
+        if color_temp < 3000 or color_temp > max_color_temp:
             raise HuizuoException("Invalid color temperature: %s" % color_temp)
 
         return self.set_property("color_temp", color_temp)
