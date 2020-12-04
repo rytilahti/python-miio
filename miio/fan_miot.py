06c75cecda8b926dd018af0bc0efdf00f26591b1
Sebastian Muszynski <s.muszynski@constructiva.de>
2020-12-04 21:30:37 +0100

    Add Smart Standing Fan 1C (dmaker.fan.p8) support (Closes: #882)

--- miio/fan_miot.py
+++ miio/fan_miot.py
@@ -7,6 +7,7 @@ from .click_common import EnumType, command, format_output
 from .fan_common import FanException, MoveDirection, OperationMode
 from .miot_device import DeviceStatus, MiotDevice
 
+MODEL_FAN_P8 = "dmaker.fan.p8"
 MODEL_FAN_P9 = "dmaker.fan.p9"
 MODEL_FAN_P10 = "dmaker.fan.p10"
 MODEL_FAN_P11 = "dmaker.fan.p11"
@@ -23,6 +24,14 @@ MIOT_MAPPING = {
         "buzzer": {"siid": 2, "piid": 11},
         "light": {"siid": 2, "piid": 12},
         "mode": {"siid": 2, "piid": 7},
+    MODEL_FAN_P8: {
+        # Source https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:fan:0000A005:dmaker-p8:1
+        "power": {"siid": 2, "piid": 1},
+        "fan_level": {"siid": 2, "piid": 2},
+        "swing_mode": {"siid": 2, "piid": 3},
+        "mode": {"siid": 2, "piid": 7},
+        "power_off_time": {"siid": 2, "piid": 10},
+        "child_lock": {"siid": 3, "piid": 1},
     },
     MODEL_FAN_P9: {
         # Source https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:fan:0000A005:dmaker-p9:1
@@ -81,6 +90,69 @@ class OperationModeMiot(enum.Enum):
     Nature = 1
 
 
+class OperationModeP8(enum.Enum):
+    StraightWind = 0
+    Idle = 1
+
+
+class FanStatusP8(DeviceStatus):
+    """Container for status reports for Xiaomi Mi Smart Pedestal Fan DMaker P8."""
+
+    def __init__(self, data: Dict[str, Any]) -> None:
+        """
+        Response of a FanP8 (dmaker.fan.p8):
+
+        {
+          'id': 1,
+          'result': [
+            {'did': 'power', 'siid': 2, 'piid': 1, 'code': 0, 'value': False},
+            {'did': 'fan_level', 'siid': 2, 'piid': 2, 'code': 0, 'value': 2},
+            {'did': 'swing_mode', 'siid': 2, 'piid': 4, 'code': 0, 'value': False},
+            {'did': 'mode', 'siid': 2, 'piid': 3, 'code': 0, 'value': 0},
+            {'did': 'power_off_time', 'siid': 2, 'piid': 6, 'code': 0, 'value': 0},
+            {'did': 'child_lock', 'siid': 3, 'piid': 1, 'code': 0, 'value': False},
+          ],
+          'exe_time': 280
+        }
+        """
+        self.data = data
+
+    @property
+    def power(self) -> str:
+        """Power state."""
+        return "on" if self.data["power"] else "off"
+
+    @property
+    def is_on(self) -> bool:
+        """True if device is currently on."""
+        return self.data["power"]
+
+    @property
+    def mode(self) -> OperationMode:
+        """Operation mode."""
+        return OperationMode[OperationModeP8(self.data["mode"]).name]
+
+    @property
+    def speed_level(self) -> int:
+        """Returns the speed level."""
+        return self.data["fan_level"]
+
+    @property
+    def oscillate(self) -> bool:
+        """True if oscillation is enabled."""
+        return self.data["swing_mode"]
+
+    @property
+    def delay_off_countdown(self) -> int:
+        """Countdown until turning off in minutes."""
+        return self.data["power_off_time"]
+
+    @property
+    def child_lock(self) -> bool:
+        """True if child lock is on."""
+        return self.data["child_lock"]
+
+
 class FanStatusMiot(DeviceStatus):
     """Container for status reports for Xiaomi Mi Smart Pedestal Fan DMaker P9/P10."""
 
@@ -127,6 +199,11 @@ class FanStatusMiot(DeviceStatus):
     def speed(self) -> int:
         """Speed of the motor."""
         return self.data["fan_speed"]
+
+    @property
+    def speed_level(self) -> int:
+        """Returns the speed level."""
+        return self.data["fan_level"]
 
     @property
     def oscillate(self) -> bool:
@@ -229,7 +306,107 @@ class FanStatus1C(DeviceStatus):
         return self.data["child_lock"]
 
 
-class FanMiot(MiotDevice):
+class FanP8(MiotDevice):
+    def __init__(
+        self,
+        ip: str = None,
+        token: str = None,
+        start_id: int = 0,
+        debug: int = 0,
+        lazy_discover: bool = True,
+        model: str = MODEL_FAN_P8,
+    ) -> None:
+        if model not in MIOT_MAPPING:
+            raise FanException("Invalid FanMiot model: %s" % model)
+
+        super().__init__(ip, token, start_id, debug, lazy_discover)
+        self.model = model
+
+    @command(
+        default_output=format_output(
+            "",
+            "Power: {result.power}\n"
+            "Operation mode: {result.mode}\n"
+            "Speed level: {result.speed_level}\n"
+            "Oscillate: {result.oscillate}\n"
+            "Child lock: {result.child_lock}\n"
+            "Power-off time: {result.delay_off_countdown}\n",
+        )
+    )
+    def status(self) -> FanStatusP8:
+        """Retrieve properties."""
+        return FanStatusP8(
+            {
+                prop["did"]: prop["value"] if prop["code"] == 0 else None
+                for prop in self.get_properties_for_mapping()
+            }
+        )
+
+    @command(default_output=format_output("Powering on"))
+    def on(self):
+        """Power on."""
+        return self.set_property("power", True)
+
+    @command(default_output=format_output("Powering off"))
+    def off(self):
+        """Power off."""
+        return self.set_property("power", False)
+
+    @command(
+        click.argument("mode", type=EnumType(OperationMode)),
+        default_output=format_output("Setting mode to '{mode.value}'"),
+    )
+    def set_mode(self, mode: OperationMode):
+        """Set mode."""
+        return self.set_property("mode", OperationModeP8[mode.name].value)
+
+    @command(
+        click.argument("speed_level", type=int),
+        default_output=format_output("Setting speed to {speed_level}"),
+    )
+    def set_speed_level(self, speed_level: int):
+        """Set speed level."""
+        if speed not in (1, 2, 3):
+            raise FanException("Invalid speed level: %s" % speed)
+
+        return self.set_property("fan_level", speed_level)
+
+    @command(
+        click.argument("oscillate", type=bool),
+        default_output=format_output(
+            lambda oscillate: "Turning on oscillate"
+            if oscillate
+            else "Turning off oscillate"
+        ),
+    )
+    def set_oscillate(self, oscillate: bool):
+        """Set oscillate on/off."""
+        return self.set_property("swing_mode", oscillate)
+
+    @command(
+        click.argument("lock", type=bool),
+        default_output=format_output(
+            lambda lock: "Turning on child lock" if lock else "Turning off child lock"
+        ),
+    )
+    def set_child_lock(self, lock: bool):
+        """Set child lock on/off."""
+        return self.set_property("child_lock", lock)
+
+    @command(
+        click.argument("minutes", type=int),
+        default_output=format_output("Setting delayed turn off to {minutes} minutes"),
+    )
+    def delay_off(self, minutes: int):
+        """Set delay off minutes."""
+
+        if minutes < 0 or minutes > 480:
+            raise FanException("Invalid value for a delayed turn off: %s" % minutes)
+
+        return self.set_property("power_off_time", minutes)
+
+
+class FanMiot(FanP8):
     mapping = MIOT_MAPPING[MODEL_FAN_P10]
 
     def __init__(
@@ -241,11 +418,11 @@ class FanMiot(MiotDevice):
         lazy_discover: bool = True,
         model: str = MODEL_FAN_P10,
     ) -> None:
-        if model not in MIOT_MAPPING:
+        if model in MIOT_MAPPING:
+            self.model = model
+        else:
             raise FanException("Invalid FanMiot model: %s" % model)
-
-        super().__init__(ip, token, start_id, debug, lazy_discover)
-        self.model = model
+        super().__init__(MIOT_MAPPING[model], ip, token, start_id, debug, lazy_discover)
 
     @command(
         default_output=format_output(
@@ -269,16 +446,6 @@ class FanMiot(MiotDevice):
                 for prop in self.get_properties_for_mapping()
             }
         )
-
-    @command(default_output=format_output("Powering on"))
-    def on(self):
-        """Power on."""
-        return self.set_property("power", True)
-
-    @command(default_output=format_output("Powering off"))
-    def off(self):
-        """Power off."""
-        return self.set_property("power", False)
 
     @command(
         click.argument("mode", type=EnumType(OperationMode)),
@@ -314,21 +481,6 @@ class FanMiot(MiotDevice):
         return self.set_property("swing_mode_angle", angle)
 
     @command(
-        click.argument("oscillate", type=bool),
-        default_output=format_output(
-            lambda oscillate: "Turning on oscillate"
-            if oscillate
-            else "Turning off oscillate"
-        ),
-    )
-    def set_oscillate(self, oscillate: bool):
-        """Set oscillate on/off."""
-        if oscillate:
-            return self.set_property("swing_mode", True)
-        else:
-            return self.set_property("swing_mode", False)
-
-    @command(
         click.argument("led", type=bool),
         default_output=format_output(
             lambda led: "Turning on LED" if led else "Turning off LED"
@@ -336,10 +488,7 @@ class FanMiot(MiotDevice):
     )
     def set_led(self, led: bool):
         """Turn led on/off."""
-        if led:
-            return self.set_property("light", True)
-        else:
-            return self.set_property("light", False)
+        return self.set_property("light", led)
 
     @command(
         click.argument("buzzer", type=bool),
@@ -349,32 +498,7 @@ class FanMiot(MiotDevice):
     )
     def set_buzzer(self, buzzer: bool):
         """Set buzzer on/off."""
-        if buzzer:
-            return self.set_property("buzzer", True)
-        else:
-            return self.set_property("buzzer", False)
-
-    @command(
-        click.argument("lock", type=bool),
-        default_output=format_output(
-            lambda lock: "Turning on child lock" if lock else "Turning off child lock"
-        ),
-    )
-    def set_child_lock(self, lock: bool):
-        """Set child lock on/off."""
-        return self.set_property("child_lock", lock)
-
-    @command(
-        click.argument("minutes", type=int),
-        default_output=format_output("Setting delayed turn off to {minutes} minutes"),
-    )
-    def delay_off(self, minutes: int):
-        """Set delay off minutes."""
-
-        if minutes < 0 or minutes > 480:
-            raise FanException("Invalid value for a delayed turn off: %s" % minutes)
-
-        return self.set_property("power_off_time", minutes)
+        return self.set_property("buzzer", buzzer)
 
     @command(
         click.argument("direction", type=EnumType(MoveDirection)),
@@ -458,7 +582,7 @@ class Fan1C(MiotDevice):
     def set_speed(self, speed: int):
         """Set speed."""
         if speed not in (1, 2, 3):
-            raise FanException("Invalid speed: %s" % speed)
+            raise FanException("Invalid speed level: %s" % speed)
 
         return self.set_property("fan_level", speed)
 
