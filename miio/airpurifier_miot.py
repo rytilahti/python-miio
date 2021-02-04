84051d11dde2a2044f735b8c7d91c5eb7a8d0a78
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-02-04 13:22:27 +0100

    Add comment

--- miio/airpurifier_miot.py
+++ miio/airpurifier_miot.py
@@ -84,6 +84,8 @@ class AirPurifierMiotStatus:
     @property
     def aqi(self) -> int:
         """Air quality index."""
+        # zhimi-airpurifier-mb3 returns 1 as AQI value if the measurement was 
+        # unsuccessful
         if self.data["aqi"] == 1:
             return None
         return self.data["aqi"]
