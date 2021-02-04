2970a944d787b1060ed96d8e7307ad76786130b1
starkillerOG <starkiller.og@gmail.com>
2021-02-04 13:54:27 +0100

    fix issort

--- miio/gateway/devices/__init__.py
+++ miio/gateway/devices/__init__.py
@@ -2,7 +2,7 @@
 
 # flake8: noqa
 from .light import LightBulb
-from .switch import Switch
 from .sensor import Vibration
+from .switch import Switch
 
 from .subdevice import SubDevice, SubDeviceInfo  # isort:skip
