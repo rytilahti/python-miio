188d9611cd8b3ff1bbb570ab6014b23ab1e76787
starkillerOG <starkiller.og@gmail.com>
2021-09-04 12:07:10 +0200

    Do not get battery status for mains powered devices

--- miio/gateway/devices/subdevice.py
+++ miio/gateway/devices/subdevice.py
@@ -41,6 +41,7 @@ class SubDevice:
         if model_info is None:
             model_info = {}
         self._model_info = model_info
+        self._battery_powered = model_info.get("battery", True)
         self._battery = None
         self._voltage = None
         self._fw_ver = dev_info.fw_ver
@@ -208,6 +209,9 @@ class SubDevice:
     @command()
     def get_battery(self):
         """Update the battery level, if available."""
+        if not self._battery_powered:
+            return self._battery
+
         if self._gw.model not in [GATEWAY_MODEL_EU, GATEWAY_MODEL_ZIG3]:
             self._battery = self.send("get_battery").pop()
         else:
@@ -220,6 +224,9 @@ class SubDevice:
     @command()
     def get_voltage(self):
         """Update the battery voltage, if available."""
+        if not self._battery_powered:
+            return self._voltage
+
         if self._gw.model in [GATEWAY_MODEL_EU, GATEWAY_MODEL_ZIG3]:
             self._voltage = self.get_property("voltage").pop() / 1000
         else:
