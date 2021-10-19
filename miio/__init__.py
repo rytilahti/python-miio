136e2e920f2914809e965eaab503b4dd9f10ef23
Teemu Rytilahti <tpr@iki.fi>
2021-10-19 21:56:27 +0200

    Fix the import

--- miio/__init__.py
+++ miio/__init__.py
@@ -41,6 +41,7 @@ from miio.gateway import Gateway
 from miio.heater import Heater
 from miio.heater_miot import HeaterMiot
 from miio.huizuo import Huizuo, HuizuoLampFan, HuizuoLampHeater, HuizuoLampScene
+from miio.integrations.yeelight import Yeelight
 from miio.miot_device import MiotDevice
 from miio.philips_bulb import PhilipsBulb, PhilipsWhiteBulb
 from miio.philips_eyecare import PhilipsEyecare
@@ -68,7 +69,6 @@ from miio.waterpurifier import WaterPurifier
 from miio.waterpurifier_yunmi import WaterPurifierYunmi
 from miio.wifirepeater import WifiRepeater
 from miio.wifispeaker import WifiSpeaker
-from miio.yeelight import Yeelight
 from miio.yeelight_dual_switch import YeelightDualControlModule
 
 from miio.discovery import Discovery
