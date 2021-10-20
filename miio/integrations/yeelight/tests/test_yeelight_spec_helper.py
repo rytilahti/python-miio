4419a61c7dc59c0b3626fad4e761d244d11ee13a
Kirmas <krosven@outlook.com>
2021-10-20 18:53:55 +0300

    Remove class from test

--- miio/integrations/yeelight/tests/test_yeelight_spec_helper.py
+++ miio/integrations/yeelight/tests/test_yeelight_spec_helper.py
@@ -1,20 +1,19 @@
-from unittest import TestCase
-
 from ..spec_helper import YeelightSpecHelper
 
 
-class TestYeelightSpecHelper(TestCase):
-    def test_get_model_info(self):
-        model_info = YeelightSpecHelper.get_model_info("yeelink.light.bslamp1")
-        assert model_info.model == "yeelink.light.bslamp1"
-        assert model_info.color_temp == (1700, 6500)
-        assert model_info.has_night_light is False
-        assert model_info.has_background_light is False
-        assert model_info.supports_color is True
+def test_get_model_info():
+    model_info = YeelightSpecHelper.get_model_info("yeelink.light.bslamp1")
+    assert model_info.model == "yeelink.light.bslamp1"
+    assert model_info.color_temp == (1700, 6500)
+    assert model_info.has_night_light is False
+    assert model_info.has_background_light is False
+    assert model_info.supports_color is True
 
-        model_info = YeelightSpecHelper.get_model_info("notreal")
-        assert model_info.model == "generic"
-        assert model_info.color_temp == (1700, 6500)
-        assert model_info.has_night_light is False
-        assert model_info.has_background_light is False
-        assert model_info.supports_color is False
+
+def test_get_unknown_model_info():
+    model_info = YeelightSpecHelper.get_model_info("notreal")
+    assert model_info.model == "generic"
+    assert model_info.color_temp == (1700, 6500)
+    assert model_info.has_night_light is False
+    assert model_info.has_background_light is False
+    assert model_info.supports_color is False
