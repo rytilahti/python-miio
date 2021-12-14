ee88751fc29f123fc311f79c966b9e13cbcd4eae
starkillerOG <starkiller.og@gmail.com>
2021-12-14 16:39:47 +0100

    gateway: remove click support for gateway devices (#1229)
    * fix gateway devices
    
    * Do not inherit from Device on gatewaydevices

--- miio/gateway/zigbee.py
+++ miio/gateway/zigbee.py
@@ -1,30 +1,23 @@
 """Xiaomi Gateway Zigbee control implementation."""
 
-import click
-
-from ..click_common import command
 from .gatewaydevice import GatewayDevice
 
 
 class Zigbee(GatewayDevice):
     """Zigbee controls."""
 
-    @command()
     def get_zigbee_version(self):
         """timeouts on device."""
         return self._gateway.send("get_zigbee_device_version")
 
-    @command()
     def get_zigbee_channel(self):
         """Return currently used zigbee channel."""
         return self._gateway.send("get_zigbee_channel")[0]
 
-    @command(click.argument("channel"))
     def set_zigbee_channel(self, channel):
         """Set zigbee channel."""
         return self._gateway.send("set_zigbee_channel", [channel])
 
-    @command(click.argument("timeout", type=int))
     def zigbee_pair(self, timeout):
         """Start pairing, use 0 to disable."""
         return self._gateway.send("start_zigbee_join", [timeout])
@@ -52,7 +45,6 @@ class Zigbee(GatewayDevice):
         raise NotImplementedError()
         return self._gateway.send("write_zigbee_attribute")
 
-    @command()
     def zigbee_unpair_all(self):
         """Unpair all devices."""
         return self._gateway.send("remove_all_device")
