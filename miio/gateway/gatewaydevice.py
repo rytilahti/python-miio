f893ce0471a9ce719bf48a7536c099b5c2a69700
starkillerOG <starkiller.og@gmail.com>
2021-12-14 11:57:24 +0100

    Do not inherit from Device on gatewaydevices

--- miio/gateway/gatewaydevice.py
+++ miio/gateway/gatewaydevice.py
@@ -3,7 +3,6 @@
 import logging
 from typing import TYPE_CHECKING
 
-from ..device import Device
 from ..exceptions import DeviceException
 
 _LOGGER = logging.getLogger(__name__)
@@ -13,17 +12,12 @@ if TYPE_CHECKING:
     from .gateway import Gateway
 
 
-class GatewayDevice(Device):
+class GatewayDevice:
     """GatewayDevice class Specifies the init method for all gateway device
     functionalities."""
 
     def __init__(
         self,
-        ip: str = None,
-        token: str = None,
-        start_id: int = 0,
-        debug: int = 0,
-        lazy_discover: bool = True,
         parent: "Gateway" = None,
     ) -> None:
         if parent is None:
@@ -32,5 +26,3 @@ class GatewayDevice(Device):
             )
 
         self._gateway = parent
-        self.ip = self._gateway.ip
-        self.token = self._gateway.token
