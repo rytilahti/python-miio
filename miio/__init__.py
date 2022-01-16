8cf8572f5d697ad5304895afe82d7c21769aaeda
Teemu Rytilahti <tpr@iki.fi>
2022-01-16 19:59:47 +0100

    Move philips light implementations to integrations/light/philips

--- miio/__init__.py
+++ miio/__init__.py
@@ -46,6 +46,13 @@ from miio.heater_miot import HeaterMiot
 from miio.huizuo import Huizuo, HuizuoLampFan, HuizuoLampHeater, HuizuoLampScene
 from miio.integrations.fan.dmaker import Fan1C, FanMiot, FanP5, FanP9, FanP10, FanP11
 from miio.integrations.fan.zhimi import Fan, FanZA5
+from miio.integrations.light.philips import (
+    PhilipsBulb,
+    PhilipsEyecare,
+    PhilipsMoonlight,
+    PhilipsRwread,
+    PhilipsWhiteBulb,
+)
 from miio.integrations.petwaterdispenser import PetWaterDispenser
 from miio.integrations.vacuum.dreame.dreamevacuum_miot import DreameVacuumMiot
 from miio.integrations.vacuum.mijia import G1Vacuum
@@ -61,10 +68,6 @@ from miio.integrations.vacuum.roborock.vacuumcontainers import (
 from miio.integrations.vacuum.roidmi.roidmivacuum_miot import RoidmiVacuumMiot
 from miio.integrations.vacuum.viomi.viomivacuum import ViomiVacuum
 from miio.integrations.yeelight import Yeelight
-from miio.philips_bulb import PhilipsBulb, PhilipsWhiteBulb
-from miio.philips_eyecare import PhilipsEyecare
-from miio.philips_moonlight import PhilipsMoonlight
-from miio.philips_rwread import PhilipsRwread
 from miio.powerstrip import PowerStrip
 from miio.protocol import Message, Utils
 from miio.pwzn_relay import PwznRelay
