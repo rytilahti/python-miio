4ab8c5e435289d28601f933ab66d8b86b8a67fe6
Alexander Pitkin <peleccom@gmail.com>
2022-02-01 00:01:44 +0300

    Dreame increase code coverage

--- miio/integrations/vacuum/dreame/tests/test_dreamevacuum_miot.py
+++ miio/integrations/vacuum/dreame/tests/test_dreamevacuum_miot.py
@@ -44,6 +44,12 @@ _INITIAL_STATE_1C = {
     "volume": 4,
     "voice_package": "DE",
     "timezone": "Europe/London",
+    "cleaning_time": 10,
+    "cleaning_area": 20,
+    "first_clean_time": 1640854830,
+    "total_clean_time": 1000,
+    "total_clean_times": 15,
+    "total_clean_area": 500,
 }
 
 
@@ -71,6 +77,12 @@ _INITIAL_STATE_F9 = {
     "water_flow": 2,
     "water_box_carriage_status": 1,
     "timezone": "Europe/London",
+    "cleaning_time": 10,
+    "cleaning_area": 20,
+    "first_clean_time": 1640854830,
+    "total_clean_time": 1000,
+    "total_clean_times": 15,
+    "total_clean_area": 500,
 }
 
 
@@ -110,6 +122,13 @@ class TestDreame1CVacuum(TestCase):
         assert status.filter_left_time == _INITIAL_STATE_1C["filter_left_time"]
         assert status.filter_life_level == _INITIAL_STATE_1C["filter_life_level"]
         assert status.timezone == _INITIAL_STATE_1C["timezone"]
+        assert status.cleaning_time == _INITIAL_STATE_1C["cleaning_time"]
+        assert status.cleaning_area == _INITIAL_STATE_1C["cleaning_area"]
+        assert status.first_clean_time == _INITIAL_STATE_1C["first_clean_time"]
+        assert status.total_clean_time == _INITIAL_STATE_1C["total_clean_time"]
+        assert status.total_clean_times == _INITIAL_STATE_1C["total_clean_times"]
+        assert status.total_clean_area == _INITIAL_STATE_1C["total_clean_area"]
+
         assert status.device_fault == FaultStatus(_INITIAL_STATE_1C["device_fault"])
         assert repr(status.device_fault) == repr(
             FaultStatus(_INITIAL_STATE_1C["device_fault"])
@@ -146,6 +165,16 @@ class TestDreame1CVacuum(TestCase):
         assert status.volume == _INITIAL_STATE_1C["volume"]
         assert status.voice_package == _INITIAL_STATE_1C["voice_package"]
 
+    def test_fanspeed_presets(self):
+        presets = self.device.fan_speed_presets()
+        for item in CleaningModeDreame1C:
+            assert item.name in presets
+            assert presets[item.name] == item.value
+
+    def test_fan_speed(self):
+        value = self.device.fan_speed()
+        assert value == {"Medium": 2}
+
 
 @pytest.mark.usefixtures("dummydreamef9vacuum")
 class TestDreameF9Vacuum(TestCase):
@@ -160,6 +189,12 @@ class TestDreameF9Vacuum(TestCase):
         assert status.filter_life_level == _INITIAL_STATE_F9["filter_life_level"]
         assert status.water_flow == WaterFlow(_INITIAL_STATE_F9["water_flow"])
         assert status.timezone == _INITIAL_STATE_F9["timezone"]
+        assert status.cleaning_time == _INITIAL_STATE_1C["cleaning_time"]
+        assert status.cleaning_area == _INITIAL_STATE_1C["cleaning_area"]
+        assert status.first_clean_time == _INITIAL_STATE_1C["first_clean_time"]
+        assert status.total_clean_time == _INITIAL_STATE_1C["total_clean_time"]
+        assert status.total_clean_times == _INITIAL_STATE_1C["total_clean_times"]
+        assert status.total_clean_area == _INITIAL_STATE_1C["total_clean_area"]
         assert status.is_water_box_carriage_attached
         assert status.device_fault == FaultStatus(_INITIAL_STATE_F9["device_fault"])
         assert repr(status.device_fault) == repr(
@@ -193,3 +228,23 @@ class TestDreameF9Vacuum(TestCase):
         assert status.map_view == _INITIAL_STATE_F9["map_view"]
         assert status.volume == _INITIAL_STATE_F9["volume"]
         assert status.voice_package == _INITIAL_STATE_F9["voice_package"]
+
+    def test_fanspeed_presets(self):
+        presets = self.device.fan_speed_presets()
+        for item in CleaningModeDreameF9:
+            assert item.name in presets
+            assert presets[item.name] == item.value
+
+    def test_fan_speed(self):
+        value = self.device.fan_speed()
+        assert value == {"Standart": 1}
+
+    def test_waterflow_presets(self):
+        presets = self.device.waterflow_presets()
+        for item in WaterFlow:
+            assert item.name in presets
+            assert presets[item.name] == item.value
+
+    def test_waterflow(self):
+        value = self.device.waterflow()
+        assert value == {"Medium": 2}
