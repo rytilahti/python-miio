59ab4e339f8591a99613a54f8ea72ec8baec2992
Teemu Rytilahti <tpr@iki.fi>
2022-04-05 17:52:21 +0200

    Fix linting

--- miio/integrations/vacuum/dreame/dreamevacuum_miot.py
+++ miio/integrations/vacuum/dreame/dreamevacuum_miot.py
@@ -198,7 +198,7 @@ def _get_cleaning_mode_enum_class(model):
         DREAME_Z10_PRO,
         DREAME_MOP_2_PRO_PLUS,
         DREAME_MOP_2_ULTRA,
-        DREAME_MOP_2
+        DREAME_MOP_2,
     ):
         return CleaningModeDreameF9
     return None
