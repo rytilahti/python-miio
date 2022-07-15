49d8b69d22878257a83f2c2fd973f9949906939f
starkillerOG <starkiller.og@gmail.com>
2022-07-15 21:07:12 +0200

    Update miio/gateway/gateway.py
    Co-authored-by: Teemu R. <tpr@iki.fi>

--- miio/gateway/gateway.py
+++ miio/gateway/gateway.py
@@ -406,7 +406,7 @@ class Gateway(Device):
             )
         self._registered_callbacks[id] = callback
 
-    def Remove_callback(self, id):
+    def remove_callback(self, id):
         """Remove a external callback using its id."""
         self._registered_callbacks.pop(id)
 
