31484bf98e23af4967444a70a401143df7400a8b
escoand <escoand@users.noreply.github.com>
2022-09-10 22:30:41 +0200

    Update dreamevacuum_miot.py

--- miio/integrations/vacuum/dreame/dreamevacuum_miot.py
+++ miio/integrations/vacuum/dreame/dreamevacuum_miot.py
@@ -21,6 +21,7 @@ DREAME_1C = "dreame.vacuum.mc1808"
 DREAME_F9 = "dreame.vacuum.p2008"
 DREAME_D9 = "dreame.vacuum.p2009"
 DREAME_Z10_PRO = "dreame.vacuum.p2028"
+DREAME_L10_PRO = "dreame.vacuum.p2029"
 DREAME_MOP_2_PRO_PLUS = "dreame.vacuum.p2041o"
 DREAME_MOP_2_ULTRA = "dreame.vacuum.p2150a"
 DREAME_MOP_2 = "dreame.vacuum.p2150o"
@@ -121,6 +122,7 @@ _DREAME_F9_MAPPING: MiotMapping = {
 }
 
 _DREAME_TROUVER_FINDER_MAPPING: MiotMapping = {
+    # https://home.miot-spec.com/spec/dreame.vacuum.p2029
     # https://home.miot-spec.com/spec/dreame.vacuum.p2036
     "battery_level": {"siid": 3, "piid": 1},
     "charging_state": {"siid": 3, "piid": 2},
@@ -167,6 +169,7 @@ MIOT_MAPPING: Dict[str, MiotMapping] = {
     DREAME_F9: _DREAME_F9_MAPPING,
     DREAME_D9: _DREAME_F9_MAPPING,
     DREAME_Z10_PRO: _DREAME_F9_MAPPING,
+    DREAME_L10_PRO: _DREAME_TROUVER_FINDER_MAPPING,
     DREAME_MOP_2_PRO_PLUS: _DREAME_F9_MAPPING,
     DREAME_MOP_2_ULTRA: _DREAME_F9_MAPPING,
     DREAME_MOP_2: _DREAME_F9_MAPPING,
@@ -244,6 +247,7 @@ def _get_cleaning_mode_enum_class(model):
         DREAME_F9,
         DREAME_D9,
         DREAME_Z10_PRO,
+        DREAME_L10_PRO,
         DREAME_MOP_2_PRO_PLUS,
         DREAME_MOP_2_ULTRA,
         DREAME_MOP_2,
