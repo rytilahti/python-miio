38959e024fddb1b452ff608a2f9db5c068c7980b
Teemu Rytilahti <tpr@iki.fi>
2022-09-15 19:10:29 +0300

    Embed consumable status and cleaning summary for roborock vacuums

--- miio/integrations/vacuum/roborock/vacuum.py
+++ miio/integrations/vacuum/roborock/vacuum.py
@@ -403,7 +403,10 @@ class RoborockVacuum(Device, VacuumInterface):
     @command()
     def status(self) -> VacuumStatus:
         """Return status of the vacuum."""
-        return VacuumStatus(self.send("get_status")[0])
+        status = VacuumStatus(self.send("get_status")[0])
+        status.embed(self.consumable_status())
+        status.embed(self.clean_history())
+        return status
 
     def enable_log_upload(self):
         raise NotImplementedError("unknown parameters")
