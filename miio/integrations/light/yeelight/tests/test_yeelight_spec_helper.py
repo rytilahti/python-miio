bf740b469150a644913b73b5f834fdbdcb9aeadd
Teemu Rytilahti <tpr@iki.fi>
2022-11-01 18:17:19 +0100

    Fix tests

--- miio/integrations/light/yeelight/tests/test_yeelight_spec_helper.py
+++ miio/integrations/light/yeelight/tests/test_yeelight_spec_helper.py
@@ -1,4 +1,8 @@
-from ..spec_helper import ColorTempRange, YeelightSpecHelper, YeelightSubLightType
+from ..spec_helper import (
+    ColorTemperatureRange,
+    YeelightSpecHelper,
+    YeelightSubLightType,
+)
 
 
 def test_get_model_info():
@@ -6,9 +10,9 @@ def test_get_model_info():
     model_info = spec_helper.get_model_info("yeelink.light.bslamp1")
     assert model_info.model == "yeelink.light.bslamp1"
     assert model_info.night_light is False
-    assert model_info.lamps[YeelightSubLightType.Main].color_temp == ColorTempRange(
-        1700, 6500
-    )
+    assert model_info.lamps[
+        YeelightSubLightType.Main
+    ].color_temp == ColorTemperatureRange(1700, 6500)
     assert model_info.lamps[YeelightSubLightType.Main].supports_color is True
     assert YeelightSubLightType.Background not in model_info.lamps
 
@@ -18,8 +22,8 @@ def test_get_unknown_model_info():
     model_info = spec_helper.get_model_info("notreal")
     assert model_info.model == "yeelink.light.*"
     assert model_info.night_light is False
-    assert model_info.lamps[YeelightSubLightType.Main].color_temp == ColorTempRange(
-        1700, 6500
-    )
+    assert model_info.lamps[
+        YeelightSubLightType.Main
+    ].color_temp == ColorTemperatureRange(1700, 6500)
     assert model_info.lamps[YeelightSubLightType.Main].supports_color is False
     assert YeelightSubLightType.Background not in model_info.lamps
