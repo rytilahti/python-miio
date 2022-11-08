1c3b463e950040e62dfd29d9d911a7394437b4bc
Teemu R <tpr@iki.fi>
2022-11-08 19:58:01 +0100

    Mark more roborock devices as supported (#1582)
    Adds also wildcard model.

--- miio/integrations/vacuum/roborock/vacuum.py
+++ miio/integrations/vacuum/roborock/vacuum.py
@@ -152,23 +152,30 @@ ROCKROBO_S5 = "roborock.vacuum.s5"
 ROCKROBO_S5_MAX = "roborock.vacuum.s5e"
 ROCKROBO_S6 = "roborock.vacuum.s6"
 ROCKROBO_T6 = "roborock.vacuum.t6"  # cn s6
+ROCKROBO_E4 = "roborock.vacuum.a01"
 ROCKROBO_S6_PURE = "roborock.vacuum.a08"
 ROCKROBO_T7 = "roborock.vacuum.a11"  # cn s7
 ROCKROBO_T7S = "roborock.vacuum.a14"
 ROCKROBO_T7SPLUS = "roborock.vacuum.a23"
 ROCKROBO_S7_MAXV = "roborock.vacuum.a27"
+ROCKROBO_S7_PRO_ULTRA = "roborock.vacuum.a62"
 ROCKROBO_Q5 = "roborock.vacuum.a34"
+ROCKROBO_Q7_MAX = "roborock.vacuum.a38"
 ROCKROBO_G10S = "roborock.vacuum.a46"
+ROCKROBO_G10 = "roborock.vacuum.a29"
+
 ROCKROBO_S7 = "roborock.vacuum.a15"
 ROCKROBO_S6_MAXV = "roborock.vacuum.a10"
 ROCKROBO_E2 = "roborock.vacuum.e2"
 ROCKROBO_1S = "roborock.vacuum.m1s"
 ROCKROBO_C1 = "roborock.vacuum.c1"
+ROCKROBO_WILD = "roborock.vacuum.*"  # wildcard
 
 SUPPORTED_MODELS = [
     ROCKROBO_V1,
     ROCKROBO_S4,
     ROCKROBO_S4_MAX,
+    ROCKROBO_E4,
     ROCKROBO_S5,
     ROCKROBO_S5_MAX,
     ROCKROBO_S6,
@@ -179,12 +186,16 @@ SUPPORTED_MODELS = [
     ROCKROBO_T7SPLUS,
     ROCKROBO_S7,
     ROCKROBO_S7_MAXV,
+    ROCKROBO_S7_PRO_ULTRA,
     ROCKROBO_Q5,
+    ROCKROBO_Q7_MAX,
+    ROCKROBO_G10,
     ROCKROBO_G10S,
     ROCKROBO_S6_MAXV,
     ROCKROBO_E2,
     ROCKROBO_1S,
     ROCKROBO_C1,
+    ROCKROBO_WILD,
 ]
 
 AUTO_EMPTY_MODELS = [
