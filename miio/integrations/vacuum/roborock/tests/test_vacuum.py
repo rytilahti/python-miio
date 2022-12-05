e7f693cdf8a6d8f46f7de73ff3f8cd5a948a4cb6
starkillerOG <starkiller.og@gmail.com>
2022-12-05 21:11:25 +0100

    fix tests

--- miio/integrations/vacuum/roborock/tests/test_vacuum.py
+++ miio/integrations/vacuum/roborock/tests/test_vacuum.py
@@ -40,6 +40,9 @@ class DummyVacuum(DummyDevice, RoborockVacuum):
         }
         self._maps = None
         self._map_enum_cache = None
+        self._floor_clean_details = {}
+        self._last_clean_details = None
+        self._searched_clean_id = None
 
         self.dummies = {}
         self.dummies["consumables"] = [
