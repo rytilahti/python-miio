615847049742bfe075be5ce9b68507f14345ecb0
Teemu Rytilahti <tpr@iki.fi>
2023-02-11 00:05:03 +0100

    Add enum for standardized vacuum identifier names

---
+++ miio/identifiers.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class VacuumId(Enum):
+    """Vacuum-specific standardized descriptor identifiers.
+
+    TODO: this is a temporary solution, and might be named to 'Vacuum' later on.
+    """
+
+    # Actions
+    Start = "vacuum:start-sweep"
+    Stop = "vacuum:stop-sweeping"
+    Pause = "vacuum:pause-sweeping"
+    Home = "battery:start-charge"
+    Locate = "identify:identify"
+
+    # Settings
+    FanSpeed = "vacuum:fan-speed"  # TODO: invented name
+    FanSpeedPreset = "vacuum:mode"
+
+    # Sensors
+    Status = "vacuum:status"
+    ErrorMessage = "vacuum:fault"
+    Battery = "battery:level"
