c9456364bece000eedbfac7b5759adcf74c07cd4
st7105 <st7105@gmail.com>
2023-05-14 12:28:49 +0300

    Fix parse properties fields without 'value'

--- miio/integrations/dreame/vacuum/dreamevacuum_miot.py
+++ miio/integrations/dreame/vacuum/dreamevacuum_miot.py
@@ -511,7 +511,7 @@ class DreameVacuum(MiotDevice):
 
         return DreameVacuumStatus(
             {
-                prop["did"]: prop["value"] if prop["code"] == 0 else None
+                prop["did"]: prop["value"] if prop["code"] == 0 and 'value' in prop else None
                 for prop in self.get_properties_for_mapping(max_properties=10)
             },
             self.model,
