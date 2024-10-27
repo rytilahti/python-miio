9c8d96a0fb37bf77b2e371a6d430101bc0257776
Teemu Rytilahti <tpr@iki.fi>
2024-10-27 02:38:13 +0200

    Use ruff for formatting
    Also, simplify the CI pipeline to use pre-commit instead of individual checks

--- miio/push_server/server.py
+++ miio/push_server/server.py
@@ -253,9 +253,7 @@ class PushServer:
         command = f"{self.server_model}.{info.action}:{source_id}"
         key = f"event.{info.source_model}.{info.event}"
         message_id = 0
-        magic_number = randint(
-            1590161094, 1642025774
-        )  # nosec, min/max taken from packet captures, unknown use
+        magic_number = randint(1590161094, 1642025774)  # nosec, min/max taken from packet captures, unknown use
 
         if len(command) > 49:
             _LOGGER.error(
